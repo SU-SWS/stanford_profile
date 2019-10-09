06b57fd0cc4d8225cb6e1e61e77d60bc8a9cd42f
Mike Decker <pookmish@gmail.com>
2019-10-09 16:43:13 -0700

    added post update to remove entity_embed

---
+++ stanford_profile.post_update.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @file
+ * stanford_profile.install
+ */
+
+use Drupal\filter\Entity\FilterFormat;
+
+/**
+ * Remove entity_embed filter after stanford_media is done updating.
+ */
+function stanford_profile_post_update_8001() {
+  /** @var FilterFormat $filter_format */
+  foreach (FilterFormat::loadMultiple() as $filter_format) {
+    $filters = $filter_format->get('filters');
+    if (isset($filters['entity_embed'])) {
+      $filter_format->filters();
+      $filter_format->removeFilter('entity_embed');
+      $filter_format->calculateDependencies();
+      $filter_format->save();
+    }
+  }
+}
