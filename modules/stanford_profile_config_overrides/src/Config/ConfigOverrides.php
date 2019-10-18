145c5491cac876b4e0a5728bdfc8d0f4c112ead4
Shea McKinney <sherakama@gmail.com>
2019-10-18 12:27:24 -0700

    DEC-152: Remove config pages entity.

--- modules/stanford_profile_config_overrides/src/Config/ConfigOverrides.php
+++ modules/stanford_profile_config_overrides/src/Config/ConfigOverrides.php
@@ -27,72 +27,17 @@ class ConfigOverrides implements ConfigFactoryOverrideInterface {
    * {@inheritDoc}
    */
   public function loadOverrides($names) {
-    $overrides = [];
-
-    // Basic Site Settings.
-    $overrides = $this->loadBasicSettingsOverrides($overrides, $names);
-
-    // Branding Options Settings.
-    $overrides = $this->loadBrandingOptionOverrides($overrides, $names);
-
-    // Pass em all back.
-    return $overrides;
-  }
-
-  /**
-   * Helper method for the loadOverrides method
-   *
-   * This function loads the overrides specific to the basic settings config.
-   * @param array  $overrides
-   *  An array of overrides.
-   * @param array $names
-   *  An array of config.
-   *
-   * @return array
-   *  An array of config override information.
-   */
-  private function loadBasicSettingsOverrides(array $overrides = [], array $names) {
-    // Basic Site Settings - Redirect Settings.
     if (in_array('domain_301_redirect.settings', $names)) {
       $site_url = $this->configPages->getValue('stanford_basic_site_settings', 'su_site_url', 0, 'uri');
 
-      $overrides['domain_301_redirect.settings'] =
-        [
+      return [
+        'domain_301_redirect.settings' => [
           'enabled' => !empty($site_url),
           'domain' => str_replace('http://', 'https://', $site_url),
-        ];
+        ],
+      ];
     }
-
-    return $overrides;
-  }
-
-  /**
-   * Helper function for the loadOverrides method.
-   *
-   * Loads overrides specifically from the branding overrides config form.
-   *
-   * @param array  $overrides
-   *  An array of overrides.
-   * @param array $names
-   *  An array of config.
-   *
-   * @return array
-   *  An array of config override information.
-   */
-  private function loadBrandingOptionOverrides(array $overrides = [], array $names) {
-
-    // Override the system setting.
-    if (in_array('system.site', $names) || in_array('stanford_basic.settings', $names)) {
-      $site_name = $this->configPages->getValue('stanford_branding_options', 'su_site_name', 0, 'value');
-      if (!empty($site_name)) {
-        // Main override.
-        $overrides['system.site'] = [ 'name' => $site_name ];
-        // And override the theme override of the system setting.
-        $overrides['stanford_basic.settings'] = [ 'line1' => $site_name ];
-      }
-    }
-
-    return $overrides;
+    return [];
   }
 
   /**
