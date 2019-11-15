d6b38e7a269440c52dab28b9f7d862983b383c29
pookmish <pookmish@stanford.edu>
2019-11-15 15:11:00 -0800

    D8CORE-826 layout library perms (#47)
    * D8CORE-826 Layout library field permission
    
    * added tests for permissions

--- stanford_profile.profile
+++ stanford_profile.profile
@@ -6,6 +6,10 @@
  */
 
 use Drupal\menu_link_content\Entity\MenuLinkContent;
+use Drupal\Core\Field\FieldDefinitionInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Access\AccessResult;
 
 /**
  * Implements hook_ENTITY_TYPE_insert().
@@ -16,4 +20,18 @@ function stanford_profile_menu_link_content_presave(MenuLinkContent $entity) {
   if ($entity->isNew()) {
     $entity->set('expanded', TRUE);
   }
+}
+
+/**
+ * Implements hook_entity_field_access().
+ */
+function stanford_profile_entity_field_access($operation, FieldDefinitionInterface $field_definition, AccountInterface $account, FieldItemListInterface $items = NULL) {
+  if ($field_definition->getType() == 'entity_reference' && $field_definition->getSetting('handler') == 'layout_library') {
+    $entity_type = $field_definition->getTargetEntityTypeId();
+    $bundle = $field_definition->getTargetBundle();
+    if (!$account->hasPermission("choose layout for $entity_type $bundle")) {
+      return AccessResult::forbidden();
+    }
+  }
+  return AccessResult::neutral();
 }
---
+++ tests/src/Kernel/StanfordProfilePermissionsTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Drupal\Tests\stanford_profile\Kernel;
+
+use Drupal\KernelTests\KernelTestBase;
+use Drupal\layout_builder\Entity\LayoutBuilderEntityViewDisplay;
+use Drupal\node\Entity\NodeType;
+use Drupal\stanford_profile\StanfordProfilePermissions;
+
+/**
+ * Class StanfordProfilePermissionsTest.
+ *
+ * @group stanford_profile
+ * @coversDefaultClass \Drupal\stanford_profile\StanfordProfilePermissions
+ */
+class StanfordProfilePermissionsTest extends KernelTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'system',
+    'node',
+    'layout_discovery',
+    'layout_builder',
+    'layout_library',
+    'path_alias',
+    'user',
+    'field',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+    $this->installSchema('system', ['key_value_expire']);
+    $this->installEntitySchema('user');
+    $this->installEntitySchema('node');
+    $this->installEntitySchema('entity_view_display');
+
+    NodeType::create(['type' => 'article', 'name' => 'Article'])->save();
+    /** @var \Drupal\Core\Entity\Display\EntityViewDisplayInterface $display */
+    $display = LayoutBuilderEntityViewDisplay::create([
+      'targetEntityType' => 'node',
+      'bundle' => 'article',
+      'mode' => 'default',
+      'status' => TRUE,
+    ])
+      ->enableLayoutBuilder()
+      ->setOverridable();
+    $display->setThirdPartySetting('layout_library', 'enable', TRUE);
+    $display->save();
+  }
+
+  /**
+   * Test permissions are returned.
+   */
+  public function testPermissions() {
+    $permission_class = StanfordProfilePermissions::create(\Drupal::getContainer());
+    $permissions = $permission_class->permissions();
+    $this->assertCount(1, $permissions);
+    $this->assertArrayHasKey('choose layout for node article', $permissions);
+  }
+
+}
