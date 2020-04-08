d6cc0145b4f421b6c5d570fa041a3e8c331b8a2e
Mike Decker <pookmish@gmail.com>
2020-04-08 10:05:33 -0700

    D8CORE-1501 Add drush command to generate stress test node

---
+++ src/Commands/StanfordProfileCommands.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Drupal\stanford_profile\Commands;
+
+use Drush\Commands\DrushCommands;
+
+/**
+ * Class StanfordProfileCommands.
+ *
+ * @package Drupal\stanford_profile\Commands
+ */
+class StanfordProfileCommands extends DrushCommands {
+
+  /**
+   * Generate a page with the possible combinations of components in rows.
+   *
+   * @command stanford-profile:stress-test-components
+   * @alias spstc
+   */
+  public function generateStressTestNode(){
+
+  }
+
+}
