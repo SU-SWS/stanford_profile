37b1423b94813c0f3d557fae2c81b330aa2e5a67
Mike Decker <pookmish@gmail.com>
2020-04-24 11:46:01 -0700

    added tests for revision delete

--- tests/codeception/acceptance/Contrib/NodeRevisionDeleteCest.php
+++ tests/codeception/acceptance/Contrib/NodeRevisionDeleteCest.php
@@ -3,10 +3,25 @@
 class NodeRevisionDeleteCest {
 
   public function testNodeRevisionDelete(AcceptanceTester $I) {
+    $I->logInWithRole('administrator');
+    /** @var \Drupal\node\NodeInterface $node */
     $node = $I->createEntity([
       'type' => 'stanford_page',
       'title' => 'revision test',
+      'revision' => TRUE,
     ]);
+    for ($j = 0; $j < 10; $j++) {
+      $node->setNewRevision();
+      $node->setRevisionLogMessage("Revision $j");
+      $node->setRevisionCreationTime(time() - ($j * 30));
+      $node->set('revision_translation_affected', TRUE);
+      $node->save();
+    }
+    $I->amOnPage("/node/{$node->id()}/revisions");
+    $I->canSeeNumberOfElements('.node-revision-table tbody tr', 11);
+    $I->runDrush('node-revision-delete stanford_page');
+    $I->amOnPage("/node/{$node->id()}/revisions");
+    $I->canSeeNumberOfElements('.node-revision-table tbody tr', 5);
   }
 
 }
