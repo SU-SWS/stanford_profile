3f9f873896b1dea1c6f7cf5e518a42481865d18a
pookmish <pookmish@stanford.edu>
2020-05-05 13:11:45 -0700

    D8CORE-1943: Tests for Fix links in the react widget (#189)
    * D8CORE-1943: Fix links in the react widget - tests

---
+++ tests/codeception/functional/BasicPage/BasicPageParagraphsCest.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Test for the basic page content type.
+ */
+class BasicPageParagraphsCest {
+
+  /**
+   * Test the card component data is displayed correctly.
+   */
+  public function testCardParagraph(FunctionalTester $I) {
+    $paragraph = $I->createEntity(['type' => 'stanford_card'], 'paragraph');
+    $node = $I->createEntity([
+      'type' => 'stanford_page',
+      'title' => 'Test Cards',
+      'su_page_components' => [
+        'target_id' => $paragraph->id(),
+        'entity' => $paragraph,
+      ],
+    ]);
+    $I->logInWithRole('contributor');
+    $I->amOnPage("/node/{$node->id()}/edit");
+    $I->waitForElementVisible('#row-0');
+    $I->click('Edit', '#row-0');
+    $I->waitForElement('.MuiAutocomplete-input');
+    $I->fillField('Subhead', 'SubHead text');
+    $I->fillField('Headline', 'Headline');
+    $I->fillField('URL', 'http://google.com');
+    $I->fillField('Link text', 'Google Link');
+    $I->click('Continue');
+    $I->waitForElementNotVisible('.MuiDialog-scrollPaper');
+    $I->wait(1);
+    $I->click('Save', '#edit-actions');
+    $I->canSee('SubHead text');
+    $I->canSee('Headline');
+    $I->canSeeLink('Google Link', 'http://google.com/');
+  }
+
+}
