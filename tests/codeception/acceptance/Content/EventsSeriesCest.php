26dfddb629082fd6bc73531a0d4d9aa329fac407
Shea McKinney <sherakama@gmail.com>
2020-06-09 10:14:11 -0700

    fixup! start tests.

---
+++ tests/codeception/acceptance/Content/EventsSeriesCest.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * Test the Event Series functionality.
+ */
+class EventsSeriesCest {
+
+  /**
+   * Ensure event and series
+   */
+  public function testXMLSiteMap(AcceptanceTester $I) {
+
+  }
+
+  /**
+   * Test thing.
+   */
+  public function testMetaDataSettings(AcceptanceTester $I) {
+
+  }
+
+  /**
+   * Test thing.
+   */
+  public function testPageTitleIgoreCondition(AcceptanceTester $I) {
+
+  }
+
+  /**
+   * Test thing.
+   */
+  public function testContentTypeExists(AcceptanceTester $I) {
+
+  }
+
+  /**
+   * Test thing.
+   */
+  public function testContributorPerms(AcceptanceTester $I) {
+
+  }
+
+  /**
+   * Test thing.
+   */
+  public function testEditorPerms(AcceptanceTester $I) {
+
+  }
+
+  /**
+   * Test thing.
+   */
+  public function testSiteOwnerPerms(AcceptanceTester $I) {
+
+  }
+
+}
