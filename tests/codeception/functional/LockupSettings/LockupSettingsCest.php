6ddba1595f110347b4cc86d4a49dcdf56cd0e3ad
boznik <boznik@boznik.com>
2020-06-16 11:36:43 -0700

    D8CORE-1472: options additions

--- tests/codeception/functional/LockupSettings/LockupSettingsCest.php
+++ tests/codeception/functional/LockupSettings/LockupSettingsCest.php
@@ -9,9 +9,33 @@ class LockupSettingsCest {
    * Test the lockup settings overrides.
    */
   public function testLockupSettings(FunctionalTester $I) {
-    $I->logInWithRole("site_manager");
-    $I->amOnPage("/admin/appearance/lockup-settings");
-    $I->selectOption("#edit-xmlsitemap-status", "a");
+    $I->logInWithRole('site_manager');
+    $I->amOnPage('/');
+    $I->seeElement('.su-lockup');
+
+  }
+
+  /**
+   * Test the lockup settings overrides.
+   */
+  public function testLockupSettingsA(FunctionalTester $I) {
+    $I->logInWithRole('site_manager');
+    $I->amOnPage('/admin/appearance/lockup-settings');
+    $I->selectOption('#edit-su-lockup-options', 'a');
+    $I->fillField('Line 1', 'Site title line');
+    $I->fillField('Line 2', 'Secondary title line');
+    $I->fillField('Line 3', 'Tertiary title line');
+    $I->fillField('Line 4', 'Organization name');
+    $I->fillField('Line 5', 'Last line full width option');
+  }
+
+  /**
+   * Test the lockup settings overrides.
+   */
+  public function testLockupSettingsB(FunctionalTester $I) {
+    $I->logInWithRole('site_manager');
+    $I->amOnPage('/admin/appearance/lockup-settings');
+    $I->selectOption("#edit-su-lockup-options", "b");
     $I->fillField('Line 1', 'Site title line');
     $I->fillField('Line 2', 'Secondary title line');
     $I->fillField('Line 3', 'Tertiary title line');
