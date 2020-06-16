57ca42fa46c5d09cf00fdf959b7a5cde09a1344f
boznik <boznik@boznik.com>
2020-06-16 16:13:31 -0700

    D8CORE-1472: test lockup options vs css config

--- tests/codeception/functional/LockupSettings/LockupSettingsCest.php
+++ tests/codeception/functional/LockupSettings/LockupSettingsCest.php
@@ -6,7 +6,7 @@
 class LockupSettingsCest {
 
   /**
-   * Test the lockup settings overrides.
+   * Test the lockup exists.
    */
   public function testLockupSettings(FunctionalTester $I) {
     $I->logInWithRole('site_manager');
@@ -27,6 +27,11 @@ class LockupSettingsCest {
     $I->fillField('Line 3', 'Tertiary title line');
     $I->fillField('Line 4', 'Organization name');
     $I->fillField('Line 5', 'Last line full width option');
+    $I->click('Save');
+    $I->amOnPage('/');
+    $I->waitForElement('.su-lockup', 5);
+    $I->canSee("Site title line");
+    $I->canSee("Last line full width option");
   }
 
   /**
@@ -41,6 +46,193 @@ class LockupSettingsCest {
     $I->fillField('Line 3', 'Tertiary title line');
     $I->fillField('Line 4', 'Organization name');
     $I->fillField('Line 5', 'Last line full width option');
+    $I->click('Save');
+    $I->amOnPage('/');
+    $I->canSee("Site title line");
+    $I->canSee("Secondary title line");
+  }
+
+  /**
+   * Test the lockup settings overrides.
+   */
+  public function testLockupSettingsD(FunctionalTester $I) {
+    $I->logInWithRole('site_manager');
+    $I->amOnPage('/admin/appearance/lockup-settings');
+    $I->selectOption("#edit-su-lockup-options", "d");
+    $I->fillField('Line 1', 'Site title line');
+    $I->fillField('Line 2', 'Secondary title line');
+    $I->fillField('Line 3', 'Tertiary title line');
+    $I->fillField('Line 4', 'Organization name');
+    $I->fillField('Line 5', 'Last line full width option');
+    $I->click('Save');
+    $I->amOnPage('/');
+    $I->canSee("Site title line");
+    $I->canSee("Tertiary title line");
+  }
+
+  /**
+   * Test the lockup settings overrides.
+   */
+  public function testLockupSettingsE(FunctionalTester $I) {
+    $I->logInWithRole('site_manager');
+    $I->amOnPage('/admin/appearance/lockup-settings');
+    $I->selectOption("#edit-su-lockup-options", "e");
+    $I->fillField('Line 1', 'Site title line');
+    $I->fillField('Line 2', 'Secondary title line');
+    $I->fillField('Line 3', 'Tertiary title line');
+    $I->fillField('Line 4', 'Organization name');
+    $I->fillField('Line 5', 'Last line full width option');
+    $I->click('Save');
+    $I->amOnPage('/');
+    $I->canSee("Site title line");
+    $I->canSee("Secondary title line");
+    $I->canSee("Tertiary title line");
+  }
+
+  /**
+   * Test the lockup settings overrides.
+   */
+  public function testLockupSettingsH(FunctionalTester $I) {
+    $I->logInWithRole('site_manager');
+    $I->amOnPage('/admin/appearance/lockup-settings');
+    $I->selectOption("#edit-su-lockup-options", "h");
+    $I->fillField('Line 1', 'Site title line');
+    $I->fillField('Line 2', 'Secondary title line');
+    $I->fillField('Line 3', 'Tertiary title line');
+    $I->fillField('Line 4', 'Organization name');
+    $I->fillField('Line 5', 'Last line full width option');
+    $I->click('Save');
+    $I->amOnPage('/');
+    $I->canSee("Site title line");
+    $I->canSee("Organization name");
+    $I->canSee("Tertiary title line");
+  }
+
+  /**
+   * Test the lockup settings overrides.
+   */
+  public function testLockupSettingsI(FunctionalTester $I) {
+    $I->logInWithRole('site_manager');
+    $I->amOnPage('/admin/appearance/lockup-settings');
+    $I->selectOption("#edit-su-lockup-options", "i");
+    $I->fillField('Line 1', 'Site title line');
+    $I->fillField('Line 2', 'Secondary title line');
+    $I->fillField('Line 3', 'Tertiary title line');
+    $I->fillField('Line 4', 'Organization name');
+    $I->fillField('Line 5', 'Last line full width option');
+    $I->click('Save');
+    $I->amOnPage('/');
+    $I->canSee("Site title line");
+    $I->canSee("Organization name");
+    $I->canSee("Tertiary title line");
+  }
+
+  /**
+   * Test the lockup settings overrides.
+   */
+  public function testLockupSettingsK(FunctionalTester $I) {
+    $I->logInWithRole('site_manager');
+    $I->amOnPage('/admin/appearance/lockup-settings');
+    $I->selectOption("#edit-su-lockup-options", "k");
+    $I->fillField('Line 1', 'Site title line');
+    $I->fillField('Line 2', 'Secondary title line');
+    $I->fillField('Line 3', 'Tertiary title line');
+    $I->fillField('Line 4', 'Organization name');
+    $I->fillField('Line 5', 'Last line full width option');
+    $I->click('Save');
+    $I->amOnPage('/');
+    $I->canSee("Site title line");
+    $I->canSee("Last line full width option");
+  }
+
+  /**
+   * Test the lockup settings overrides.
+   */
+  public function testLockupSettingsO(FunctionalTester $I) {
+    $I->logInWithRole('site_manager');
+    $I->amOnPage('/admin/appearance/lockup-settings');
+    $I->selectOption("#edit-su-lockup-options", "o");
+    $I->fillField('Line 1', 'Site title line');
+    $I->fillField('Line 2', 'Secondary title line');
+    $I->fillField('Line 3', 'Tertiary title line');
+    $I->fillField('Line 4', 'Organization name');
+    $I->fillField('Line 5', 'Last line full width option');
+    $I->click('Save');
+    $I->amOnPage('/');
+    $I->canSee("Organization name");
+  }
+
+  /**
+   * Test the lockup settings overrides.
+   */
+  public function testLockupSettingsP(FunctionalTester $I) {
+    $I->logInWithRole('site_manager');
+    $I->amOnPage('/admin/appearance/lockup-settings');
+    $I->selectOption("#edit-su-lockup-options", "p");
+    $I->fillField('Line 1', 'Site title line');
+    $I->fillField('Line 2', 'Secondary title line');
+    $I->fillField('Line 3', 'Tertiary title line');
+    $I->fillField('Line 4', 'Organization name');
+    $I->fillField('Line 5', 'Last line full width option');
+    $I->click('Save');
+    $I->amOnPage('/');
+    $I->canSee("Site title line");
+    $I->canSee("Organization name");
+  }
+
+  /**
+   * Test the lockup settings overrides.
+   */
+  public function testLockupSettingsR(FunctionalTester $I) {
+    $I->logInWithRole('site_manager');
+    $I->amOnPage('/admin/appearance/lockup-settings');
+    $I->selectOption("#edit-su-lockup-options", "r");
+    $I->fillField('Line 1', 'Site title line');
+    $I->fillField('Line 2', 'Secondary title line');
+    $I->fillField('Line 3', 'Tertiary title line');
+    $I->fillField('Line 4', 'Organization name');
+    $I->fillField('Line 5', 'Last line full width option');
+    $I->click('Save');
+    $I->amOnPage('/');
+    $I->canSee("Last line full width option");
+  }
+
+  /**
+   * Test the lockup settings overrides.
+   */
+  public function testLockupSettingsS(FunctionalTester $I) {
+    $I->logInWithRole('site_manager');
+    $I->amOnPage('/admin/appearance/lockup-settings');
+    $I->selectOption("#edit-su-lockup-options", "s");
+    $I->fillField('Line 1', 'Site title line');
+    $I->fillField('Line 2', 'Secondary title line');
+    $I->fillField('Line 3', 'Tertiary title line');
+    $I->fillField('Line 4', 'Organization name');
+    $I->fillField('Line 5', 'Last line full width option');
+    $I->click('Save');
+    $I->amOnPage('/');
+    $I->canSee("Site title line");
+    $I->canSee("Secondary title line");
+    $I->canSee("Organization name");
+  }
+
+  /**
+   * Test the lockup settings overrides.
+   */
+  public function testLockupSettingsT(FunctionalTester $I) {
+    $I->logInWithRole('site_manager');
+    $I->amOnPage('/admin/appearance/lockup-settings');
+    $I->selectOption("#edit-su-lockup-options", "t");
+    $I->fillField('Line 1', 'Site title line');
+    $I->fillField('Line 2', 'Secondary title line');
+    $I->fillField('Line 3', 'Tertiary title line');
+    $I->fillField('Line 4', 'Organization name');
+    $I->fillField('Line 5', 'Last line full width option');
+    $I->click('Save');
+    $I->canSee("Site title line");
+    $I->canSee("Secondary title line");
+    $I->canSee("Tertiary title line");
+    $I->canSee("Organization name");
   }
 
 }
