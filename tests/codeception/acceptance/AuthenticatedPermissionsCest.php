9acd199d6a50cdd6b3d6ad2ba13d99a6d382c031
Ian Monroe <ian@ianmonroe.com>
2020-06-23 16:05:06 -0600

    D8CORE-1241: linting and cleanup.

--- tests/codeception/acceptance/AuthenticatedPermissionsCest.php
+++ tests/codeception/acceptance/AuthenticatedPermissionsCest.php
@@ -1,42 +1,44 @@
 <?php
 
-
 /**
  * Test the restrictions on authenticated users.
  */
 class AuthenticatedPermissionsCest {
 
-    /**
-     * Make sure we can't see admin pages.
-     */
-    public function testAuthenticatedUserRestrictions(AcceptanceTester $I) {
-      $I->logInWithRole('authenticated');
-      $I->amOnPage('/');
-      $I->canSeeResponseCodeIs(200);
-      $I->amOnPage('/admin');
-      $I->canSeeResponseCodeIs(403);
-      $I->amOnPage('/admin/content');
-      $I->canSeeResponseCodeIs(403);
-      $I->amOnPage('/admin/structure');
-      $I->canSeeResponseCodeIs(403);
-      $I->amOnPage('/admin/appearance');
-      $I->canSeeResponseCodeIs(403);
-      $I->amOnPage('/admin/modules');
-      $I->canSeeResponseCodeIs(403);
-      $I->amOnPage('/admin/config');
-      $I->canSeeResponseCodeIs(403);
-      $I->amOnPage('/admin/people');
-      $I->canSeeResponseCodeIs(403);
-      $I->amOnPage('/admin/reports');
-      $I->canSeeResponseCodeIs(403);
-      $I->amOnPage('/admin/reports/status');
-      $I->canSeeResponseCodeIs(403);
-    }
-
-    public function testAuthenticatedUserPermissions(AcceptanceTester $I) {
-      $I->logInWithRole('authenticated');
-      $I->amOnPage('/patterns');
-      $I->canSeeResponseCodeIs(200);
-    }
-
+  /**
+   * Make sure authenticated users can't access things they should not.
+   */
+  public function testAuthenticatedUserRestrictions(AcceptanceTester $I) {
+    $I->logInWithRole('authenticated');
+    $I->amOnPage('/');
+    $I->canSeeResponseCodeIs(200);
+    $I->amOnPage('/admin');
+    $I->canSeeResponseCodeIs(403);
+    $I->amOnPage('/admin/content');
+    $I->canSeeResponseCodeIs(403);
+    $I->amOnPage('/admin/structure');
+    $I->canSeeResponseCodeIs(403);
+    $I->amOnPage('/admin/appearance');
+    $I->canSeeResponseCodeIs(403);
+    $I->amOnPage('/admin/modules');
+    $I->canSeeResponseCodeIs(403);
+    $I->amOnPage('/admin/config');
+    $I->canSeeResponseCodeIs(403);
+    $I->amOnPage('/admin/people');
+    $I->canSeeResponseCodeIs(403);
+    $I->amOnPage('/admin/reports');
+    $I->canSeeResponseCodeIs(403);
+    $I->amOnPage('/admin/reports/status');
+    $I->canSeeResponseCodeIs(403);
   }
+
+  /**
+   * Make sure authenticated users can access things they should.
+   */
+  public function testAuthenticatedUserPermissions(AcceptanceTester $I) {
+    $I->logInWithRole('authenticated');
+    $I->amOnPage('/patterns');
+    $I->canSeeResponseCodeIs(200);
+  }
+
+}
