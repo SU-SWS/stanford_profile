ab6ccf689867c3d85fb79e2663f8336f780fc544
Ian Monroe <ian@ianmonroe.com>
2020-07-21 11:38:55 -0600

    D8CORE-1241: boilerplate for additional tests

--- tests/codeception/acceptance/AuthenticatedPermissionsCest.php
+++ tests/codeception/acceptance/AuthenticatedPermissionsCest.php
@@ -41,4 +41,30 @@ class AuthenticatedPermissionsCest {
     $I->canSeeResponseCodeIs(200);
   }
 
+  /**
+   * Site Manager cannot escalate own role above Site Manager
+   * Site Manager cannot escalate the role of others above Site Manager
+   * PHP entered in a redirect gets sanitized
+   * PHP code gets sanitized on content creation
+   * Try to upload a PHP file in Media and it fails form validation
+   * Try to upload a PHP file as a favicon in theme settings and have it fail (error message for me was "For security reasons, your upload has been renamed to foo.php.txt.")
+   * Try to upload a PHP file as a logo in theme settings and have it fail (error message for me was "The image file is invalid or the image type is not allowed. Allowed types: gif, jpe, jpeg, jpg, png")
+   */
+
+   public function testSiteManagerEscalationSelf(AcceptanceTester $I) {
+
+   }
+
+   public function testSiteManagerEscalationOthers(AcceptanceTester $I) {}
+
+   public function testPhpInRedirect(AcceptanceTester $I) {}
+
+   public function testPhpInContent(AcceptanceTester $I) {}
+
+   public function testPhpUploadInMedia(AcceptanceTester $I) {}
+
+   public function testPhpUploadInFavicon(AcceptanceTester $I) {}
+
+   public function testPhpUploadInLogo(AcceptanceTester $I) {}
+
 }
