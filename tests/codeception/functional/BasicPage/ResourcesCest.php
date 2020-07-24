7c7e0d169bff7059ba7bbea9aead36547a3e1208
pookmish <pookmish@stanford.edu>
2020-07-23 17:18:24 -0700

    CSD-224 Add fields and view for resources tagging (#66)
    * CSD-224 Add fields and view for resources tagging
    
    * Added missing module dependency
    
    * added tests for resources
    
    * Added resource icon to the page
    
    * renamed configs to have prefix
    
    * fixed test
    
    * restored missing view

---
+++ tests/codeception/functional/BasicPage/ResourcesCest.php
@@ -0,0 +1,87 @@
+<?php
+
+/**
+ * Class ResourcesCest.
+ *
+ * @group basic_page
+ */
+class ResourcesCest {
+
+  /**
+   * Create some resource pages and make sure they display on the list.
+   */
+  public function testResources(FunctionalTester $I) {
+    $dimension = $I->createEntity([
+      'vid' => 'su_opportunity_dimension',
+      'name' => 'Foo',
+    ], 'taxonomy_term');
+
+    $type = $I->createEntity([
+      'vid' => 'cs_resource_type',
+      'name' => 'Foo',
+    ], 'taxonomy_term');
+    $foo_aud = $I->createEntity([
+      'vid' => 'cs_resource_audience',
+      'name' => 'Foo',
+    ], 'taxonomy_term');
+    $bar_aud = $I->createEntity([
+      'vid' => 'cs_resource_audience',
+      'name' => 'Bar',
+    ], 'taxonomy_term');
+
+    $I->createEntity([
+      'type' => 'stanford_page',
+      'title' => 'This Foo Page',
+      'su_page_resource_audience' => $foo_aud->id(),
+      'su_page_resource_type' => $type->id(),
+      'su_page_resource_dimension' => $dimension->id(),
+    ]);
+    $I->createEntity([
+      'type' => 'stanford_page',
+      'title' => 'This Bar Page',
+      'su_page_resource_audience' => $bar_aud->id(),
+      'su_page_resource_type' => $type->id(),
+      'su_page_resource_dimension' => $dimension->id(),
+    ]);
+
+    $paragraph = $I->createEntity([
+      'type' => 'stanford_resource_list',
+      'su_resource_list' => [
+        'target_id' => 'cs_resources',
+        'display_id' => 'audience',
+        'arguments' => 'Foo',
+      ],
+    ], 'paragraph');
+
+    $resources = $I->createEntity([
+      'type' => 'stanford_page',
+      'title' => 'Resources',
+      'su_page_components' => [
+        'target_id' => $paragraph->id(),
+        'entity' => $paragraph,
+        'settings' => json_encode([
+          'row' => 0,
+          'index' => 0,
+          'width' => 12,
+          'admin_title' => 'Resources',
+        ]),
+      ],
+    ]);
+
+    $I->amOnPage($resources->toUrl()->toString());
+    $I->canSeeLink('This Foo Page');
+    $I->cantSeeLink('This Bar Page');
+
+    $paragraph->set('su_resource_list', [
+      'target_id' => 'cs_resources',
+      'display_id' => 'all_list',
+    ])->save();
+
+    $I->logInWithRole('site_manager');
+    $I->amOnPage($resources->toUrl('edit-form')->toString());
+    $I->click('Save');
+    $I->canSeeLink('This Foo Page');
+    $I->canSeeLink('This Bar Page');
+  }
+
+}
