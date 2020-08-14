98881c26c066384e09c5e1ff1e8f180140784ec6
Shea McKinney <sherakama@gmail.com>
2020-08-14 16:48:57 -0700

    Always seed your tests.

--- tests/codeception/functional/Contrib/ExtLinkCest.php
+++ tests/codeception/functional/Contrib/ExtLinkCest.php
@@ -13,22 +13,30 @@ class ExtLinkCest {
     // Local footer block isnt showing up on circle for some reason.
     $I->logInWithRole('site_manager');
     $I->amOnPage('/admin/config/system/local-footer');
-    $I->checkOption('Enabled');
+    $I->checkOption('#edit-su-footer-enabled-value');
+    $I->click('#edit-group-primary-links summary');
+    $I->click('#edit-group-secondary-links summary');
     $fields = [
       'su_local_foot_primary[0][uri]' => 'https://google.com',
       'su_local_foot_primary[0][title]' => 'Primary Link',
+      'su_local_foot_primary[1][uri]' => 'https://stanford.edu',
+      'su_local_foot_primary[1][title]' => 'Another primary link',
       'su_local_foot_second[0][uri]' => 'https://stanford.edu',
       'su_local_foot_second[0][title]' => 'Secondary Link',
+      'su_local_foot_second[1][uri]' => 'https://google.com',
+      'su_local_foot_second[1][title]' => 'Another secondary link',
     ];
     foreach ($fields as $selector => $value) {
       $I->fillField($selector, $value);
     }
 
     $I->click('Save');
+    $I->waitForAjaxToFinish();
     $I->runDrush('cache-rebuild');
 
     // Validate email links.
     $I->amOnPage('/');
+    $I->waitForAjaxToFinish();
     $mails = $I->grabMultiple('a.mailto svg.mailto');
     $I->assertEquals(count($mails), 3);
 
@@ -38,7 +46,7 @@ class ExtLinkCest {
 
     // External links in the local footer.
     $footerExternals = $I->grabMultiple('.su-local-footer__cell2 a.su-link--external svg.su-link--external');
-    $I->assertEquals(count($footerExternals), 2);
+    $I->assertEquals(count($footerExternals), 4);
   }
 
 }
