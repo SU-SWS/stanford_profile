493c57f795a10de2880d1245982a73f2ae12d784
Shea McKinney <sherakama@gmail.com>
2020-08-14 16:34:17 -0700

    Always seed your tests.

--- tests/codeception/functional/Contrib/ExtLinkCest.php
+++ tests/codeception/functional/Contrib/ExtLinkCest.php
@@ -9,13 +9,26 @@ class ExtLinkCest {
    * Test external links get the added class and svg.
    */
   public function testExtLink(FunctionalTester $I) {
-    $I->amOnPage('/');
 
     // Local footer block isnt showing up on circle for some reason.
-    $I->runDrush('cim -y');
-    $I->runDrush('cr');
+    $I->logInWithRole('site_manager');
+    $I->amOnPage('/admin/config/system/local-footer');
+    $I->checkOption('Enabled');
+    $fields = [
+      'su_local_foot_primary[0][uri]' => 'https://google.com',
+      'su_local_foot_primary[0][title]' => 'Primary Link',
+      'su_local_foot_second[0][uri]' => 'https://stanford.edu',
+      'su_local_foot_second[0][title]' => 'Secondary Link',
+    ];
+    foreach ($fields as $selector => $value) {
+      $I->fillField($selector, $value);
+    }
+
+    $I->click('Save');
+    $I->runDrush('cache-rebuild');
 
     // Validate email links.
+    $I->amOnPage('/');
     $mails = $I->grabMultiple('a.mailto svg.mailto');
     $I->assertEquals(count($mails), 3);
 
@@ -25,7 +38,7 @@ class ExtLinkCest {
 
     // External links in the local footer.
     $footerExternals = $I->grabMultiple('.su-local-footer__cell2 a.su-link--external svg.su-link--external');
-    $I->assertEquals(count($footerExternals), 4);
+    $I->assertEquals(count($footerExternals), 2);
   }
 
 }
