1577812d754138ba44ec68baa43ed6064afefecb
Mike Decker <pookmish@gmail.com>
2020-08-24 15:10:34 -0700

    use wait for text

--- tests/codeception/functional/BasicPage/BasicPageParagraphsCest.php
+++ tests/codeception/functional/BasicPage/BasicPageParagraphsCest.php
@@ -31,7 +31,7 @@ class BasicPageParagraphsCest {
     $I->amOnPage("/node/{$node->id()}/edit");
     $I->waitForElementVisible('#row-0');
     $I->click('Edit', '#row-0');
-    $I->waitForElementVisible('.MuiAutocomplete-input');
+    $I->waitForText('Superhead');
     $I->fillField('Superhead', 'Superhead text');
     $I->fillField('Headline', 'Headline');
     $I->fillField('URL', 'http://google.com');
