668a4eba886ea750a8e108c4e4b460fd2c203e2e
Shea McKinney <sherakama@gmail.com>
2020-09-04 00:43:57 -0700

    Fixup! local footer lockup settings.

--- tests/codeception/acceptance/SuperFooter/SuperFooterCest.php
+++ tests/codeception/acceptance/SuperFooter/SuperFooterCest.php
@@ -38,7 +38,6 @@ class SuperFooterCest {
     $I->fillField('#edit-su-super-foot-intranet-0-uri', 'https://stanford.edu/');
     $I->fillField('#edit-su-super-foot-intranet-0-title', 'Intranet Link');
     $I->click('Save');
-    $I->canSee("Super Footer Super Footer has been updated.");
     $I->amOnPage("/");
     $I->seeElement(".block-config-pages-super-footer");
     $I->canSee("Super Footer Title");
