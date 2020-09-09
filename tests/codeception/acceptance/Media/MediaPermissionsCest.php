174266b2ac7c4142163160dbb52fe804af6a8696
Shea McKinney <sherakama@gmail.com>
2020-09-09 12:24:44 -0700

    D8CORE-2521: Media embeds test.

--- tests/codeception/acceptance/Media/MediaPermissionsCest.php
+++ tests/codeception/acceptance/Media/MediaPermissionsCest.php
@@ -6,10 +6,47 @@
 class MediaPermissionsCest {
 
   /**
-   * Documents can be embedded as links.
+   * Test admin perms
    */
-  public function testFileLinks(AcceptanceTester $I) {
+  public function testAdminPerms(AcceptanceTester $I) {
     $I->logInWithRole('administrator');
+    $I->amOnPage('/media/add/embeddable');
+    $I->canSeeResponseCodeIs(200);
+    $I->canSee('oEmbed URL');
+    $I->canSee('Embed Code');
+  }
+
+  /**
+   * Test site manager perms
+   */
+  public function testSiteManagerPerms(AcceptanceTester $I) {
+    $I->logInWithRole('site_manager');
+    $I->amOnPage('/media/add/embeddable');
+    $I->canSeeResponseCodeIs(200);
+    $I->canSee('oEmbed URL');
+    $I->cantSee('Embed Code');
+  }
+
+  /**
+   * Test site editor perms
+   */
+  public function testSiteEditorPerms(AcceptanceTester $I) {
+    $I->logInWithRole('site_editor');
+    $I->amOnPage('/media/add/embeddable');
+    $I->canSeeResponseCodeIs(200);
+    $I->canSee('oEmbed URL');
+    $I->cantSee('Embed Code');
+  }
+
+  /**
+   * Test contributor perms
+   */
+  public function testContributorPerms(AcceptanceTester $I) {
+    $I->logInWithRole('contributor');
+    $I->amOnPage('/media/add/embeddable');
+    $I->canSeeResponseCodeIs(200);
+    $I->canSee('oEmbed URL');
+    $I->cantSee('Embed Code');
   }
 
 }
