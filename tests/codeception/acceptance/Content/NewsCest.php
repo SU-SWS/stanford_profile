c55f522fa31743eaa95d1baaf8fee7ce7844d85b
pookmish <pookmish@stanford.edu>
2020-11-05 13:34:24 -0800

    D8CORE-2570 Add intro block above event and news lists (#324)
    * D8CORE-2570 Add intro block above event and news lists
    
    * removed ids

--- tests/codeception/acceptance/Content/NewsCest.php
+++ tests/codeception/acceptance/Content/NewsCest.php
@@ -1,9 +1,25 @@
 <?php
 
+use Faker\Factory;
+
 /**
  * Test the news functionality.
  */
 class NewsCest {
+
+  /**
+   * News list intro block is at the top of the page.
+   */
+  public function testListIntro(AcceptanceTester $I){
+    $intro_text = Factory::create()->text();
+    $I->logInWithRole('site_manager');
+    $I->amOnPage('/news');
+    $I->click('Edit Block Content Above');
+    $I->click('Add Text Area');
+    $I->fillField('Body', $intro_text);
+    $I->click('Save');
+    $I->canSee($intro_text);
+  }
 
   /**
    * Test that the default content has installed and is unpublished.
