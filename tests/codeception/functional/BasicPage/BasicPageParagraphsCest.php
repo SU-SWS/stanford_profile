3d873ea7d8f8b4c2d466ec588b9322d880b3c514
Mike Decker <pookmish@gmail.com>
2020-11-09 16:02:13 -0800

    D8CORE-2428 Provide media library cardinality validation

--- tests/codeception/functional/BasicPage/BasicPageParagraphsCest.php
+++ tests/codeception/functional/BasicPage/BasicPageParagraphsCest.php
@@ -45,4 +45,44 @@ class BasicPageParagraphsCest {
     $I->canSeeLink('Google Link', 'http://google.com/');
   }
 
+  /**
+   * If a user searches in the media library, cardinality is checked.
+   */
+  public function testMultipleImagesD8Core2428(FunctionalTester $I) {
+    $I->logInWithRole('contributor');
+    $I->amOnPage('/node/add/stanford_page');
+    // Give it a few seconds to apply the media library javscript.
+    $I->wait(3);
+    $I->click('Add Top Banner');
+    $I->waitForText('No media items are selected.');
+    $I->click('Add media', 'form');
+    $I->waitForText('Add or select media');
+    $disabled = $I->grabAttributeFrom('[name="media_library_select_form[1]"]', 'disabled');
+    $I->assertFalse((bool)$disabled);
+
+    $I->checkOption('media_library_select_form[0]');
+    $disabled = $I->grabAttributeFrom('[name="media_library_select_form[1]"]', 'disabled');
+    $I->assertTrue((bool)$disabled);
+
+    $I->fillField('Name', 'banner');
+    $I->click('Apply filters');
+    $I->waitForAjaxToFinish();
+
+    $I->checkOption('media_library_select_form[0]');
+    $I->fillField('Name', '');
+    $I->click('Apply filters');
+    $I->waitForAjaxToFinish();
+
+    $I->canSee('2 of 1 item selected');
+    $I->click('Insert selected', '.ui-dialog-buttonset');
+    $I->waitForAjaxToFinish();
+    $I->canSeeElement('.messages.messages--error');
+
+    $I->uncheckOption('media_library_select_form[0]');
+    $I->click('Insert selected', '.ui-dialog-buttonset');
+
+    $I->waitForAjaxToFinish();
+    $I->canSeeNumberOfElements('.media-library-item__preview img', 1);
+  }
+
 }
