c69e05b387ec09101c217c3b1789c1ef98468071
pookmish <pookmish@stanford.edu>
2020-11-20 11:56:19 -0800

    D9 Ready (#329)

--- tests/src/Kernel/StanfordProfilePermissionsTest.php
+++ tests/src/Kernel/StanfordProfilePermissionsTest.php
@@ -32,7 +32,7 @@ class StanfordProfilePermissionsTest extends KernelTestBase {
   /**
    * {@inheritdoc}
    */
-  protected function setUp() {
+  protected function setUp(): void {
     parent::setUp();
     $this->installSchema('system', ['key_value_expire']);
     $this->installEntitySchema('user');
