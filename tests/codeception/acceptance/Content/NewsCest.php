c9d1b823b36e2076b81eb703c6d5368ac1bc5a70
Mike Decker <pookmish@gmail.com>
2020-12-08 15:02:45 -0800

    Dont remove special characters

--- tests/codeception/acceptance/Content/NewsCest.php
+++ tests/codeception/acceptance/Content/NewsCest.php
@@ -140,4 +140,18 @@ class NewsCest {
     $I->canSeeResponseCodeIs(200);
   }
 
+  /**
+   * Special characters should stay.
+   */
+  public function testSpecialCharacters(AcceptanceTester $I){
+    $faker = Factory::create();
+    $I->logInWithRole('contributor');
+    $I->amOnPage('/node/add/stanford_person');
+    $I->fillField('First Namme', 'Foo');
+    $I->fillField('Last Name', 'Bar-Baz & Foo');
+    $I->fillField('Short Title', $faker->text);
+    $I->click('Save');
+    $I->canSee('Foo Bar-Baz & Foo', 'h1');
+  }
+
 }
