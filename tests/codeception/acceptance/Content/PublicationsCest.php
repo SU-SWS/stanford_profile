4535ec65e4dbb35ad1804b9005c5e53e928f790f
Mike Decker <pookmish@gmail.com>
2021-01-21 22:20:29 -0800

    adjusted test

--- tests/codeception/acceptance/Content/PublicationsCest.php
+++ tests/codeception/acceptance/Content/PublicationsCest.php
@@ -18,101 +18,20 @@ class PublicationsCest {
     $I->amOnPage('/node/add/stanford_publication');
     $I->fillField('Title', 'Test Publication');
     $I->selectOption('su_publication_citation[actions][bundle]', 'Book');
-    $I->click('Add new citation');
+    $I->click('Add new Citation');
     $I->fillField('First Name', $faker->firstName);
     $I->fillField('Last Name', $faker->lastName);
-    $I->fillField('Day', $faker->numberBetween(1, 28));
-    $I->fillField('Month', $faker->numberBetween(1, 12));
-    $I->fillField('Year', $faker->numberBetween(1900, 2020));
+    $I->fillField('Subtitle', $faker->text);
+    $I->fillField('Publication Place', $faker->text);
     $I->fillField('Publisher', $faker->text);
-    $I->fillField('Volume', $faker->numberBetween(1, 10));
-    $I->fillField('Page Number(s)', $faker->numberBetween(1, 99));
-    $I->fillField('DOI/ISBN/Database Name', $faker->text);
+    $I->fillField('Month', $faker->numberBetween(1, 12));
+    $I->fillField('Day', $faker->numberBetween(1, 28));
+    $I->fillField('Year', $faker->numberBetween(1900, 2020));
     $I->fillField('su_publication_cta[0][uri]', $faker->url);
     $I->fillField('Link text', $faker->text);
 
-    $I->fillField('External Link', $faker->url);
     $I->click('Save');
     $I->canSee('Test Publication', 'h1');
   }
 
-  /**
-   * @group testme
-   */
-  public function testJournalCitation(AcceptanceTester $I) {
-    $faker = Factory::create();
-    $I->logInWithRole('contributor');
-    $I->amOnPage('/node/add/stanford_publication');
-    $I->fillField('Title', 'Test Publication');
-    $I->selectOption('su_publication_citation[actions][bundle]', 'Book');
-    $I->click('Add new citation');
-    $I->fillField('First Name', $faker->firstName);
-    $I->fillField('Last Name', $faker->lastName);
-    $I->fillField('Day', $faker->numberBetween(1, 28));
-    $I->fillField('Month', $faker->numberBetween(1, 12));
-    $I->fillField('Year', $faker->numberBetween(1900, 2020));
-    $I->fillField('Publisher', $faker->text);
-    $I->fillField('Volume', $faker->numberBetween(1, 10));
-    $I->fillField('Page Number(s)', $faker->numberBetween(1, 99));
-    $I->fillField('DOI/ISBN/Database Name', $faker->text);
-    $I->fillField('su_publication_cta[0][uri]', $faker->url);
-    $I->fillField('Link text', $faker->text);
-
-    $I->fillField('External Link', $faker->url);
-    $I->click('Save');
-    $I->canSee('Test Publication', 'h1');
-  }
-  /**
-   * @group testme
-   */
-  public function testArticleCitation(AcceptanceTester $I) {
-    $faker = Factory::create();
-    $I->logInWithRole('contributor');
-    $I->amOnPage('/node/add/stanford_publication');
-    $I->fillField('Title', 'Test Publication');
-    $I->selectOption('su_publication_citation[actions][bundle]', 'Book');
-    $I->click('Add new citation');
-    $I->fillField('First Name', $faker->firstName);
-    $I->fillField('Last Name', $faker->lastName);
-    $I->fillField('Day', $faker->numberBetween(1, 28));
-    $I->fillField('Month', $faker->numberBetween(1, 12));
-    $I->fillField('Year', $faker->numberBetween(1900, 2020));
-    $I->fillField('Publisher', $faker->text);
-    $I->fillField('Volume', $faker->numberBetween(1, 10));
-    $I->fillField('Page Number(s)', $faker->numberBetween(1, 99));
-    $I->fillField('DOI/ISBN/Database Name', $faker->text);
-    $I->fillField('su_publication_cta[0][uri]', $faker->url);
-    $I->fillField('Link text', $faker->text);
-
-    $I->fillField('External Link', $faker->url);
-    $I->click('Save');
-    $I->canSee('Test Publication', 'h1');
-  }
-
-  /**
-   * @group testme
-   */
-  public function testThesisCitation(AcceptanceTester $I) {
-    $faker = Factory::create();
-    $I->logInWithRole('contributor');
-    $I->amOnPage('/node/add/stanford_publication');
-    $I->fillField('Title', 'Test Publication');
-    $I->selectOption('su_publication_citation[actions][bundle]', 'Book');
-    $I->click('Add new citation');
-    $I->fillField('First Name', $faker->firstName);
-    $I->fillField('Last Name', $faker->lastName);
-    $I->fillField('Day', $faker->numberBetween(1, 28));
-    $I->fillField('Month', $faker->numberBetween(1, 12));
-    $I->fillField('Year', $faker->numberBetween(1900, 2020));
-    $I->fillField('Publisher', $faker->text);
-    $I->fillField('Volume', $faker->numberBetween(1, 10));
-    $I->fillField('Page Number(s)', $faker->numberBetween(1, 99));
-    $I->fillField('DOI/ISBN/Database Name', $faker->text);
-    $I->fillField('su_publication_cta[0][uri]', $faker->url);
-    $I->fillField('Link text', $faker->text);
-
-    $I->fillField('External Link', $faker->url);
-    $I->click('Save');
-    $I->canSee('Test Publication', 'h1');
-  }
 }
