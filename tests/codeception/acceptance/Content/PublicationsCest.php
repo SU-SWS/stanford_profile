91159e993aaf9be5fc3e3900654f567f090d52a5
Mike Decker <pookmish@gmail.com>
2021-02-18 15:38:11 -0800

    Tests

--- tests/codeception/acceptance/Content/PublicationsCest.php
+++ tests/codeception/acceptance/Content/PublicationsCest.php
@@ -13,10 +13,16 @@ class PublicationsCest {
    * @group testme
    */
   public function testBookCitation(AcceptanceTester $I) {
+    $I->createEntity([
+      'vid' => 'stanford_publication_topics',
+      'name' => 'Foo Bar',
+    ], 'taxonomy_term');
+
     $faker = Factory::create();
     $I->logInWithRole('contributor');
     $I->amOnPage('/node/add/stanford_publication');
     $I->fillField('Title', 'Test Publication');
+    $I->fillField('Publication Topic Terms (value 1)', 'Foo Bar');
     $I->selectOption('su_publication_citation[actions][bundle]', 'Book');
     $I->click('Add Citation');
     $I->fillField('First Name', $faker->firstName);
@@ -30,6 +36,17 @@ class PublicationsCest {
 
     $I->click('Save');
     $I->canSee('Test Publication', 'h1');
+  }
+
+  /**
+   * Test out the list pages.
+   */
+  public function testAllPublicationListPage(AcceptanceTester $I) {
+    $this->testBookCitation($I);
+    $I->amOnPage('/publications');
+    $I->canSee('Test Publication');
+    $I->click('Foo Bar');
+    $I->canSee('Test Publication');
   }
 
 }
