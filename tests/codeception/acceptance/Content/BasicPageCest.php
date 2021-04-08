ef6ad12671bf8fd05d6192168db0ebd3017decd0
Ian Monroe <ian@ianmonroe.com>
2021-04-08 11:55:00 -0600

    Test for page description

--- tests/codeception/acceptance/Content/BasicPageCest.php
+++ tests/codeception/acceptance/Content/BasicPageCest.php
@@ -67,4 +67,22 @@ class BasicPageCest {
     $I->canSeeResponseCodeIs(200);
   }
 
+  /**
+   * There should be a page description field
+   */
+  public function testPageDescription(AcceptanceTester $I) {
+    $faker = Factory::create();
+    $title = $faker->text(20);
+    $description = $faker->text(100);
+    $I->logInWithRole('site_manager');
+    $I->amOnPage('/node/add/stanford_page');
+    $I->see('Page Metadata');
+    $I->see('Page Image');
+    $I->see('Basic Page Type');
+    $I->fillField('Title', $title);
+    $I->fillField('Page Description', $description);
+    $I->click('Save');
+    $I->canSee($description);
+  }
+
 }
