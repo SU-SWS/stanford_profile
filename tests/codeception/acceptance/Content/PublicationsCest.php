5a81417107628e49e8e572f1123d417d5828028c
Mike Decker <pookmish@gmail.com>
2021-08-02 16:28:45 -0700

    D8CORE-4350 Sort publications by month and day along with year

--- tests/codeception/acceptance/Content/PublicationsCest.php
+++ tests/codeception/acceptance/Content/PublicationsCest.php
@@ -78,23 +78,82 @@ class PublicationsCest {
   /**
    * An "Other" publication type should be available.
    */
-  public function testOtherPublication(AcceptanceTester $I){
-      $faker = Factory::create();
-      $I->logInWithRole('site_manager');
-      $I->amOnPage('/node/add/stanford_publication');
-      $I->fillField('Title', 'Test Publication');
-      $I->selectOption('su_publication_citation[actions][bundle]', 'Other');
-      $I->click('Add Citation');
-      $I->fillField('First Name', $faker->firstName);
-      $I->fillField('Last Name/Company', $faker->lastName);
-      $I->fillField('Subtitle', $faker->text);
-      $I->fillField('Publisher', $faker->text);
-      $I->fillField('su_publication_cta[0][uri]', $faker->url);
-      $I->fillField('Link text', $faker->text);
+  public function testOtherPublication(AcceptanceTester $I) {
+    $faker = Factory::create();
+    $I->logInWithRole('site_manager');
+    $I->amOnPage('/node/add/stanford_publication');
+    $I->fillField('Title', 'Test Publication');
+    $I->selectOption('su_publication_citation[actions][bundle]', 'Other');
+    $I->click('Add Citation');
+    $I->fillField('First Name', $faker->firstName);
+    $I->fillField('Last Name/Company', $faker->lastName);
+    $I->fillField('Subtitle', $faker->text);
+    $I->fillField('Publisher', $faker->text);
+    $I->fillField('su_publication_cta[0][uri]', $faker->url);
+    $I->fillField('Link text', $faker->text);
 
-      $I->click('Save');
-      $I->canSee('Test Publication', 'h1');
-      $I->canSee('Publication', '.node-stanford-publication-citation-type');
+    $I->click('Save');
+    $I->canSee('Test Publication', 'h1');
+    $I->canSee('Publication', '.node-stanford-publication-citation-type');
+  }
+
+  /**
+   * Publication list should be in date order.
+   * @group mikes
+   */
+  public function testListSort(AcceptanceTester $I) {
+    $I->logInWithRole('site_manager');
+
+    $I->amOnPage('/node/add/stanford_publication');
+    $I->fillField('Title', 'A June 1st Pub');
+    $I->selectOption('su_publication_citation[actions][bundle]', 'Other');
+    $I->click('Add Citation');
+    $I->fillField('Year', 2020);
+    $I->fillField('Month', 6);
+    $I->fillField('Day', 1);
+    $I->click('Save');
+    $I->canSee('A June 1st Pub', 'h1');
+
+
+    $I->amOnPage('/node/add/stanford_publication');
+    $I->fillField('Title', 'B June 15th Pub');
+    $I->selectOption('su_publication_citation[actions][bundle]', 'Other');
+    $I->click('Add Citation');
+    $I->fillField('Year', 2020);
+    $I->fillField('Month', 6);
+    $I->fillField('Day', 15);
+    $I->click('Save');
+    $I->canSee('B June 15th Pub', 'h1');
+
+    $I->amOnPage('/node/add/stanford_publication');
+    $I->fillField('Title', 'C January Pub');
+    $I->selectOption('su_publication_citation[actions][bundle]', 'Other');
+    $I->click('Add Citation');
+    $I->fillField('Year', 2020);
+    $I->fillField('Month', 1);
+    $I->fillField('Day', 15);
+    $I->click('Save');
+    $I->canSee('C January Pub', 'h1');
+
+    $I->amOnPage('/publications');
+    $titles = $I->grabMultiple('.csl-entry a');
+    foreach($titles as &$title){
+      $title = preg_replace('/[^\da-z ]/i', '', $title);
+    }
+
+    $a_pos = array_search('A June 1st Pub', $titles);
+    $b_pos = array_search('B June 15th Pub', $titles);
+    $c_pos = array_search('C January Pub', $titles);
+
+    $I->assertGreaterThan($b_pos, $a_pos, '"A June 1st Pub" does not display after "B June 15th Pub"');
+    $I->assertGreaterThan($a_pos, $c_pos, '"C January Pub" does not display after "A June 1st Pub"');
+    $I->assertGreaterThan($b_pos, $c_pos, '"C January Pub" does not display after "B June 15th Pub"');
+
+    // Ensure distinct results
+    $titles_counts = array_count_values($titles);
+    $I->assertEquals(1, $titles_counts['A June 1st Pub']);
+    $I->assertEquals(1, $titles_counts['B June 15th Pub']);
+    $I->assertEquals(1, $titles_counts['C January Pub']);
   }
 
 }
