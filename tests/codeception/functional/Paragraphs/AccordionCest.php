17c098579fbf1ff2532f07c821e98c74ccc345eb
pookmish <pookmish@stanford.edu>
2021-08-03 07:53:19 -0700

    CAW21-82 Build accordion paragraph component (#440)
    * CAW21-82 Build accordion paragraph component

---
+++ tests/codeception/functional/Paragraphs/AccordionCest.php
@@ -0,0 +1,64 @@
+<?php
+
+use Faker\Factory;
+
+/**
+ * Class AccordionCest.
+ *
+ * @group paragraphs
+ */
+class AccordionCest {
+
+  /**
+   * Create and check the accordion.
+   */
+  public function testCreatingAccordion(FunctionalTester $I) {
+    $faker = Factory::create();
+
+    $paragraph = $I->createEntity([
+      'type' => 'stanford_accordion',
+      'su_accordion_body' => [
+        'value' => 'I can see it in your smile.',
+        'format' => 'stanford_minimal_html',
+      ],
+    ], 'paragraph');
+
+    $row = $I->createEntity([
+      'type' => 'node_stanford_page_row',
+      'su_page_components' => [
+        'target_id' => $paragraph->id(),
+        'entity' => $paragraph,
+      ],
+    ], 'paragraph_row');
+
+    $node = $I->createEntity([
+      'type' => 'stanford_page',
+      'title' => $faker->text(30),
+      'su_page_components' => [
+        'target_id' => $row->id(),
+        'entity' => $row,
+      ],
+    ]);
+
+    $I->logInWithRole('contributor');
+    $I->amOnPage($node->toUrl('edit-form')->toString());
+    $I->waitForElementVisible('#row-0');
+    $I->click('Edit', '.inner-row-wrapper');
+    $I->waitForText('Title/Question');
+    $I->fillField('Title/Question', 'Hello. Is it me you\'re looking for?');
+
+    $I->click('Continue');
+    $I->waitForElementNotVisible('.MuiDialog-scrollPaper');
+    $I->click('Save');
+
+    $I->canSee('Hello. Is it me you\'re looking for?');
+    $I->cantSeeElement('.open');
+    $open = $I->grabAttributeFrom('details', 'open');
+    $I->assertNull($open);
+    $I->clickWithLeftButton('summary');
+    $open = $I->grabAttributeFrom('details', 'open');
+    $I->assertNotNull($open);
+    $I->canSee('I can see it in your smile.');
+  }
+
+}
