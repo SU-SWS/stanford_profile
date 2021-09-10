4caf4ffc41e4e2bab603113c7a303d868406c0b2
Mike Decker <pookmish@gmail.com>
2021-09-10 10:31:45 -0700

    Configure the new embeddable validator

--- tests/codeception/acceptance/Media/MediaPermissionsCest.php
+++ tests/codeception/acceptance/Media/MediaPermissionsCest.php
@@ -24,7 +24,19 @@ class MediaPermissionsCest {
     $I->amOnPage('/media/add/embeddable');
     $I->canSeeResponseCodeIs(200);
     $I->canSee('oEmbed URL');
-    $I->cantSee('Embed Code');
+    $I->canSee('Embed Code');
+
+    $I->fillField('Name', 'Foo Bar');
+    $I->fillField('Embed Code', 'Lorem Ipsum');
+    $I->click('Save');
+    $I->canSee('The given embeddable code is not permitted.');
+    $code = [
+      '<div id="localist-widget-88041469" class="localist-widget"></div>',
+      '<script defer type="text/javascript" src="http://stanford.enterprise.localist.com/widget/view?schools=stanford&days=31&num=50&container=localist-widget-88041469&template=modern"></script>',
+    ];
+    $I->fillField('Embed Code', implode("\n", $code));
+    $I->click('Save');
+    $I->canSee('Embeddable Foo Bar has been created.');
   }
 
   /**
@@ -35,7 +47,12 @@ class MediaPermissionsCest {
     $I->amOnPage('/media/add/embeddable');
     $I->canSeeResponseCodeIs(200);
     $I->canSee('oEmbed URL');
-    $I->cantSee('Embed Code');
+    $I->canSee('Embed Code');
+
+    $I->fillField('Name', 'Foo Bar');
+    $I->fillField('Embed Code', 'Lorem Ipsum');
+    $I->click('Save');
+    $I->canSee('The given embeddable code is not permitted.');
   }
 
   /**
