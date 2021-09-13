2ad3e74a6119cf725cdc58683f5d782a6cca9a36
pookmish <pookmish@stanford.edu>
2021-09-13 08:16:35 -0700

    D8CORE-4749 Add and configure views bulk edit (#449)

---
+++ tests/codeception/acceptance/Contrib/ViewsBulkEditCest.php
@@ -0,0 +1,115 @@
+<?php
+
+use Faker\Factory;
+
+/**
+ * Test out the views bulk edit module.
+ *
+ * @group contrib
+ * @group testme
+ */
+class ViewsBulkEditCest {
+
+  /**
+   * @var \Drupal\node\NodeInterface[]
+   */
+  protected $nodes;
+
+  /**
+   * Bulk editing content changes the field values.
+   */
+  public function testBulkEdits(AcceptanceTester $I) {
+    $I->logInWithRole('site_manager');
+    $this->createEvents($I);
+    $this->createTerms($I);
+    $I->amOnPage('/admin/content');
+    $I->selectOption('Action', 'Modify field values');
+    foreach ($this->nodes as $delta => $node) {
+      $I->canSee($node->label(), 'tr');
+      $I->checkOption("views_bulk_operations_bulk_form[$delta]");
+    }
+    $I->click('Apply to selected items');
+    $I->canSee('Items selected');
+    foreach ($this->nodes as $node) {
+      $I->canSee($node->label());
+    }
+    $I->checkOption('News Topics Terms');
+    $I->fillField('node[stanford_news][su_news_topics][0][target_id]', 'Foo Bar Baz');
+    $I->checkOption('Event Type');
+    $I->selectOption('node[stanford_event][su_event_type]', 'Foo Bar Baz');
+    $I->fillField('node[stanford_event][su_event_date_time][0][value][date]', date('Y-m-d'));
+    $I->fillField('node[stanford_event][su_event_date_time][0][value][time]', '12:00:00');
+    $I->fillField('node[stanford_event][su_event_date_time][0][end_value][date]', date('Y-m-d'));
+    $I->fillField('node[stanford_event][su_event_date_time][0][end_value][time]', '12:00:00');
+
+    $I->checkOption('Publication Topic Terms');
+    $I->fillField('node[stanford_publication][su_publication_topics][0][target_id]', 'Foo Bar Baz');
+    $I->click('Apply');
+    $I->canSee('Action processing results');
+
+    foreach ($this->nodes as $node) {
+      $I->amOnPage($node->toUrl('edit-form')->toString());
+
+      switch ($node->bundle()) {
+        case 'stanford_event':
+          $I->canSeeOptionIsSelected('Event Type', 'Foo Bar Baz');
+          break;
+
+        case 'stanford_news':
+          $value = $I->grabValueFrom('[name="su_news_topics[0][target_id]"]');
+          $I->assertStringContainsString('Foo Bar Baz (', $value);
+          break;
+
+        case 'stanford_publication':
+          $value = $I->grabValueFrom('[name="su_publication_topics[0][target_id]"]');
+          $I->assertStringContainsString('Foo Bar Baz (', $value);
+          break;
+      }
+    }
+  }
+
+  /**
+   * Create a bunch of nodes.
+   *
+   * @param \AcceptanceTester $I
+   *   Tester.
+   */
+  protected function createEvents(AcceptanceTester $I) {
+    $faker = Factory::create();
+    $node_types = [
+      'stanford_event',
+      'stanford_news',
+      'stanford_publication',
+    ];
+    foreach ($node_types as $node_type) {
+      for ($j = 0; $j <= 5; $j++) {
+        $this->nodes[] = $I->createEntity([
+          'type' => $node_type,
+          'title' => $faker->text(30),
+        ]);
+      }
+    }
+  }
+
+  /**
+   * Create some taxonomy terms.
+   *
+   * @param \AcceptanceTester $I
+   *   Tester
+   */
+  protected function createTerms(AcceptanceTester $I) {
+    $I->createEntity([
+      'name' => 'Foo Bar Baz',
+      'vid' => 'stanford_event_types',
+    ], 'taxonomy_term');
+    $I->createEntity([
+      'name' => 'Foo Bar Baz',
+      'vid' => 'stanford_news_topics',
+    ], 'taxonomy_term');
+    $I->createEntity([
+      'name' => 'Foo Bar Baz',
+      'vid' => 'stanford_publication_topics',
+    ], 'taxonomy_term');
+  }
+
+}
