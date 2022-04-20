d837cc0d7fd7d7086a235944dd25493ee8ff7617
pookmish <pookmish@gmail.com>
2022-04-20 16:27:44 -0700

    D8CORE-5729 People term pages: display only child terms groupings (#526)

--- tests/codeception/acceptance/Content/PersonCest.php
+++ tests/codeception/acceptance/Content/PersonCest.php
@@ -8,6 +8,20 @@ use Faker\Factory;
 class PersonCest {
 
   /**
+   * Faker service.
+   *
+   * @var \Faker\Generator
+   */
+  protected $faker;
+
+  /**
+   * Test constructor.
+   */
+  public function __construct() {
+    $this->faker = Factory::create();
+  }
+
+  /**
    * Test that the default content has installed and is unpublished.
    */
   public function testDefaultContentExists(AcceptanceTester $I) {
@@ -161,30 +175,43 @@ class PersonCest {
 
     $faker = Factory::create();
     $parent = $I->createEntity([
-      'name' =>'Parent: '. $faker->text(10),
+      'name' => 'Parent: ' . $faker->text(10),
       'vid' => 'stanford_person_types',
     ], 'taxonomy_term');
     $child = $I->createEntity([
-      'name' => 'Child: '.$faker->text(10),
+      'name' => 'Child: ' . $faker->text(10),
       'vid' => 'stanford_person_types',
       'parent' => $parent->id(),
     ], 'taxonomy_term');
     $grandchild = $I->createEntity([
-      'name' => 'GrandChild: '.$faker->text(10),
+      'name' => 'GrandChild: ' . $faker->text(10),
       'vid' => 'stanford_person_types',
       'parent' => $child->id(),
     ], 'taxonomy_term');
     $great_grandchild = $I->createEntity([
-      'name' =>'Great GrandChild: '. $faker->text(10),
+      'name' => 'Great GrandChild: ' . $faker->text(10),
       'vid' => 'stanford_person_types',
       'parent' => $grandchild->id(),
+    ], 'taxonomy_term');
+
+    $another_parent = $I->createEntity([
+      'name' => 'Parent: ' . $faker->words(2, TRUE),
+      'vid' => 'stanford_person_types',
+    ], 'taxonomy_term');
+    $another_child = $I->createEntity([
+      'name' => 'Child: ' . $faker->words(2, TRUE),
+      'vid' => 'stanford_person_types',
+      'parent' => $another_parent->id(),
     ], 'taxonomy_term');
 
     $node = $I->createEntity([
       'type' => 'stanford_person',
       'su_person_first_name' => $faker->firstName,
       'su_person_last_name' => $faker->lastName,
-      'su_person_type_group' => $great_grandchild->id(),
+      'su_person_type_group' => [
+        ['target_id' => $great_grandchild->id()],
+        ['target_id' => $another_child->id()],
+      ],
     ]);
 
     $I->amOnPage($great_grandchild->toUrl()->toString());
@@ -195,6 +222,7 @@ class PersonCest {
     $I->canSee($node->label());
     $I->amOnPage($parent->toUrl()->toString());
     $I->canSee($node->label());
+    $I->cantSee($another_child->label());
   }
 
   /**
@@ -212,27 +240,32 @@ class PersonCest {
 
   /**
    * Unpublished profiles should not display in the list.
+   *
+   * @group tester
    */
   public function testPublishedStatus(AcceptanceTester $I) {
-    $foo = $I->createEntity([
-      'name' => 'Foo',
+    $term = $I->createEntity([
+      'name' => $this->faker->words(2, TRUE),
       'vid' => 'stanford_person_types',
     ], 'taxonomy_term');
     /** @var \Drupal\node\NodeInterface $node */
     $node = $I->createEntity([
       'type' => 'stanford_person',
-      'su_person_first_name' => "John",
-      'su_person_last_name' => "Wick",
-      'su_person_type_group' => $foo->id(),
+      'su_person_short_title' => $this->faker->title,
+      'su_person_first_name' => $this->faker->firstName,
+      'su_person_last_name' => $this->faker->lastName,
+      'su_person_type_group' => $term->id(),
     ]);
     $I->logInWithRole('administrator');
-    drupal_flush_all_caches();
-    $I->amOnPage($foo->toUrl()->toString());
-    $I->canSee($node->label());
-    $node->setUnpublished()->save();
 
-    drupal_flush_all_caches();
-    $I->amOnPage($foo->toUrl()->toString());
+    $I->amOnPage($term->toUrl()->toString());
+    $I->canSee($node->label());
+    $I->amOnPage($node->toUrl('edit-form')->toString());
+    $I->uncheckOption('Published');
+    $I->click('Save');
+    $I->canSee('page is currently unpublished');
+
+    $I->amOnPage($term->toUrl()->toString());
     $I->cantSee($node->label());
   }
 
