aa4ccf6f9e3328d653a7ca09a14f8ed38a99c3af
Joe Gilliland-Lloyd <6943710+joegl@users.noreply.github.com>
2022-04-22 15:05:39 -0500

    CS-79: Opportunities grid (#245)
    * CS-79: Added opportunity grid component and styles.
    * CS-79: Change CS opportunity view to use opportunity grid component and styled view.
    * CS-79: Added smart_trim.
    * CS-79: Updated administer opportunities view.

--- cardinal_service_profile.post_update.php
+++ cardinal_service_profile.post_update.php
@@ -21,3 +21,26 @@ function cardinal_service_profile_removed_post_updates() {
 function cardinal_service_profile_post_update_8200(){
   \Drupal::service('module_installer')->uninstall(['search']);
 }
+
+/**
+ * Set the default image for Opportunities if not set.
+ */
+function cardinal_service_profile_post_update_8201(&$sandbox) {
+  $node_storage = \Drupal::entityTypeManager()->getStorage('node');
+  $media_id = '7021';
+  if (empty($sandbox['ids'])) {
+    $sandbox['ids'] = $node_storage->getQuery()
+      ->accessCheck(FALSE)
+      ->condition('type', 'su_opportunity')
+      ->condition('su_opp_image', NULL, 'IS NULL')
+      ->execute();
+    $sandbox['total'] = count($sandbox['ids']);
+  }
+  $node_ids = array_splice($sandbox['ids'], 0, 10);
+
+  foreach ($node_storage->loadMultiple($node_ids) as $node) {
+    $node->set('su_opp_image', $media_id)->save();
+  }
+
+  $sandbox['#finished'] = count($sandbox['ids']) ? 1 - count($sandbox['ids']) / $sandbox['total'] : 1;
+}
