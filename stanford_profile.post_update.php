b0bc407c31e3225dfaad4aee2466ccbe1f942ab7
Ian Monroe <ian@ianmonroe.com>
2022-04-28 14:42:04 -0600

    D8CORE-5772: Added custom block and edit link on `/courses` page (#534)
    * D8CORE-5772: Added custom block and edit link on `/courses` page
    
    * added an additional config
    
    * Added test
    
    * Added entries for default content
    
    * Added courses intro block via update hook
    
    * D8CORE-5774: Added an "all" option to the courses menu
    
    * re-adding the default content config

--- stanford_profile.post_update.php
+++ stanford_profile.post_update.php
@@ -5,6 +5,8 @@
  * stanford_profile.install
  */
 
+use Drupal\block_content\Entity\BlockContent;
+
 /**
  * Implements hook_removed_post_updates().
  */
@@ -23,4 +25,15 @@ function stanford_profile_removed_post_updates() {
  */
 function stanford_profile_post_update_8200(){
   \Drupal::service('module_installer')->uninstall(['search']);
+}
+
+/**
+ * Create the courses intro block content.
+ */
+function stanford_profile_post_update_8201() {
+  BlockContent::create([
+    'uuid' => '2f343c04-f892-49bb-8d28-2c3f4653b02a',
+    'type' => 'stanford_component_block',
+    'info' => 'Courses Intro',
+  ])->save();
 }
