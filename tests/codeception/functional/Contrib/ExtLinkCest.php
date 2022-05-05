855f69ee717249ec43a35b452bee8482e658d184
Mike Decker <pookmish@gmail.com>
2022-05-05 06:19:11 +0000

    fixup

--- tests/codeception/functional/Contrib/ExtLinkCest.php
+++ tests/codeception/functional/Contrib/ExtLinkCest.php
@@ -1,9 +1,46 @@
 <?php
 
+use Drupal\config_pages\Entity\ConfigPages;
+
 /**
  * Test the external link module functionality.
  */
 class ExtLinkCest {
+  /**
+   * Start with a clean config page.
+   *
+   * @param \AcceptanceTester $I
+   *   Tester.
+   */
+  public function _before(FunctionalTester $I){
+    $this->_after($I);
+  }
+
+  /**
+   * Always cleanup the config after testing.
+   *
+   * @param \AcceptanceTester $I
+   *   Tester.
+   */
+  public function _after(FunctionalTester $I) {
+    if ($config_page = ConfigPages::load('stanford_local_footer')) {
+      $config_page->delete();
+    }
+
+    $config_page = ConfigPages::create([
+      'type' => 'stanford_local_footer',
+      'su_local_foot_use_loc' => TRUE,
+      'su_local_foot_use_logo' => TRUE,
+      'su_local_foot_loc_op' => 'a',
+      'context' => 'a:0:{}',
+    ]);
+    $config_page->save();
+
+    // Clean up our assets.
+    if (file_exists($this->DATA_DIR . DIRECTORY_SEPARATOR . self::LOGO_FILENAME)) {
+      unlink($this->DATA_DIR . DIRECTORY_SEPARATOR . self::LOGO_FILENAME);
+    }
+  }
 
   /**
    * Test external links get the added class and svg.
@@ -16,19 +53,20 @@ class ExtLinkCest {
     $I->checkOption('#edit-su-footer-enabled-value');
     $I->click('#edit-group-primary-links summary');
     $I->click('#edit-group-secondary-links summary');
-    $fields = [
-      'su_local_foot_primary[0][uri]' => 'https://google.com',
-      'su_local_foot_primary[0][title]' => 'Primary Link',
-      'su_local_foot_primary[1][uri]' => 'https://stanford.edu',
-      'su_local_foot_primary[1][title]' => 'Another primary link',
-      'su_local_foot_second[0][uri]' => 'https://stanford.edu',
-      'su_local_foot_second[0][title]' => 'Secondary Link',
-      'su_local_foot_second[1][uri]' => 'https://google.com',
-      'su_local_foot_second[1][title]' => 'Another secondary link',
-    ];
-    foreach ($fields as $selector => $value) {
-      $I->fillField($selector, $value);
-    }
+
+    $I->fillField('su_local_foot_primary[0][uri]', 'https://google.com');
+    $I->fillField('su_local_foot_primary[0][title]', 'Primary Link');
+    $I->click('Add another item', '.field--name-su-local-foot-primary');
+    $I->waitForElement('[name="su_local_foot_primary[1][uri]"]');
+    $I->fillField('su_local_foot_primary[1][uri]', 'https://stanford.edu');
+    $I->fillField('su_local_foot_primary[1][title]', 'Another primary link');
+
+    $I->fillField('su_local_foot_second[0][uri]', 'https://stanford.edu');
+    $I->fillField('su_local_foot_second[0][title]', 'Secondary Link');
+    $I->click('Add another item', '.field--name-su-local-foot-second');
+    $I->waitForElement('[name="su_local_foot_second[1][uri]"]');
+    $I->fillField('su_local_foot_second[1][uri]', 'https://google.com');
+    $I->fillField('su_local_foot_second[1][title]', 'Another secondary link');
 
     $I->click('Save');
 
