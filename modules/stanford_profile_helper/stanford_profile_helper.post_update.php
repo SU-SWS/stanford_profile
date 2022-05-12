56dce74e53a3efc2bb4a7cd5209c62c4b892df1e
Mike Decker <pookmish@gmail.com>
2022-05-12 13:49:10 -0700

    D8CORE-5680 Switch list landing pages to nodes with layout builder settings

--- modules/stanford_profile_helper/stanford_profile_helper.post_update.php
+++ modules/stanford_profile_helper/stanford_profile_helper.post_update.php
@@ -5,8 +5,10 @@
  * stanford_profile_helper.post_update.php
  */
 
+use Drupal\Core\Serialization\Yaml;
 use Drupal\Core\Url;
 use Drupal\block_content\Entity\BlockContent;
+use Drupal\node\NodeInterface;
 use Drupal\user\Entity\Role;
 
 /**
@@ -192,4 +194,146 @@ function stanford_profile_helper_post_update_8103(&$sandbox) {
   }
 
   $sandbox['#finished'] = count($sandbox['ids']) ? 1 - count($sandbox['ids']) / $sandbox['total'] : 1;
+}
+
+/**
+ * Create event, news, & people landing nodes.
+ */
+function stanford_profile_helper_post_update_9000() {
+  $pages = \Drupal::entityTypeManager()
+    ->getStorage('page')
+    ->loadMultiple([
+      'stanford_news_list',
+      'stanford_events_upcoming',
+      'people',
+      'courses_list_page',
+    ]);
+  foreach ($pages as $page) {
+    $page->delete();
+  }
+
+  $node_storage = \Drupal::entityTypeManager()->getStorage('node');
+  $alias_storage = \Drupal::entityTypeManager()->getStorage('path_alias');
+  \Drupal::service('router.builder')->rebuild();
+
+  $current_profile = \Drupal::config('core.extension')->get('profile');;
+  $profile_path = \Drupal::service('extension.list.profile')
+    ->getPath($current_profile);
+  /** @var \Drupal\default_content\Normalizer\ContentEntityNormalizer $normalizer */
+  $normalizer = \Drupal::service('default_content.content_entity_normalizer');
+  $pages = [
+    '8ba98fcf-d390-4014-92de-c77a59b30f3b' => [
+      'path' => '/events',
+      'type' => 'stanford_event',
+      'block' => 'f7125c85-197d-4ba2-9f6f-1126bbda0466',
+    ],
+    '0b83d1e9-688a-4475-9673-a4c385f26247' => [
+      'path' => '/news',
+      'type' => 'stanford_news',
+      'block' => '5168834f-3271-4951-bd95-e75340ca5522',
+    ],
+    '673a8fb8-39ac-49df-94c2-ed8d04db16a7' => [
+      'path' => '/people',
+      'type' => 'stanford_person',
+      'block' => 'fb905cf3-4bd3-4bcd-ad01-92d25e46ba32',
+    ],
+    '14768832-f763-4d27-8df6-7cd784886d57' => [
+      'path' => '/courses',
+      'type' => 'stanford_course',
+      'block' => '2f343c04-f892-49bb-8d28-2c3f4653b02a',
+    ],
+  ];
+
+  foreach ($pages as $uuid => $info) {
+    $number_of_nodes = $node_storage->getQuery()
+      ->accessCheck(FALSE)
+      ->condition('type', $info['type'])
+      ->count()
+      ->execute();
+
+    if ($alias_storage->loadByProperties(['alias' => $info['path']]) || !$number_of_nodes) {
+      \Drupal::messenger()
+        ->addStatus(t('Node was not created. Either @path exists or no nodes exist for that page.', ['@path' => $info['path']]));
+      continue;
+    }
+
+    $file_path = "$profile_path/content/node/$uuid.yml";
+    if (file_exists($file_path)) {
+      $decoded = Yaml::decode(file_get_contents($file_path));
+      $entity = $normalizer->denormalize($decoded);
+      $entity->save();
+      _stanford_profile_helper_add_block_contents($entity, $info['block']);
+
+      _stanford_profile_helper_fix_menu_items($entity->id(), $info['path']);
+    }
+  }
+}
+
+/**
+ * Add paragraphs and rows to the node to match the given block.
+ *
+ * @param \Drupal\node\NodeInterface $node
+ *   Node entity.
+ * @param string $block_uuid
+ *   Block entity UUID.
+ */
+function _stanford_profile_helper_add_block_contents(NodeInterface $node, $block_uuid) {
+  $block_storage = \Drupal::entityTypeManager()->getStorage('block_content');
+  $paragraph_storage = \Drupal::entityTypeManager()->getStorage('paragraph');
+  $row_storage = \Drupal::entityTypeManager()->getStorage('paragraph_row');
+
+  $block = $block_storage->loadByProperties(['uuid' => $block_uuid]);
+  if (empty($block)) {
+    return;
+  }
+
+  /** @var \Drupal\block_content\BlockContentInterface $block */
+  $block = reset($block);
+  $components = $block->get('su_component');
+  if (!$components->count()) {
+    return;
+  }
+  $rows = [];
+  /** @var \Drupal\entity_reference_revisions\Plugin\Field\FieldType\EntityReferenceRevisionsItem $component */
+  foreach ($components as $component) {
+    /** @var \Drupal\paragraphs\ParagraphInterface $paragraph */
+    $paragraph = $paragraph_storage->load($component->get('target_id')
+      ->getString());
+    $cloned_paragraph = $paragraph->createDuplicate();
+    $cloned_paragraph->enforceIsNew();
+    $cloned_paragraph->save();
+    $row = $row_storage->create([
+      'type' => 'node_stanford_page_row',
+      'su_page_components' => ['entity' => $cloned_paragraph],
+    ]);
+    $row->save();
+    $rows[] = ['entity' => $row];
+  }
+
+  $node->set('su_page_components', $rows)->save();
+}
+
+/**
+ * Find menu items and change them to target the node.
+ *
+ * @param int $node_id
+ *   Node entity id.
+ * @param string $destination_path
+ *   Aliased path of the node.
+ */
+function _stanford_profile_helper_fix_menu_items(int $node_id, string $destination_path) {
+  $menu_link_storage = \Drupal::entityTypeManager()
+    ->getStorage('menu_link_content');
+
+  $menu_link_ids = $menu_link_storage->getQuery()
+    ->accessCheck(FALSE)
+    ->condition('link', "internal:$destination_path")
+    ->execute();
+
+  /** @var \Drupal\menu_link_content\MenuLinkContentInterface $menu_link_item */
+  foreach ($menu_link_storage->loadMultiple($menu_link_ids) as $menu_link_item) {
+    $link = $menu_link_item->get('link')->get(0);
+    $link->set('uri', "internal:/node/$node_id");
+    $menu_link_item->save();
+  }
 }
