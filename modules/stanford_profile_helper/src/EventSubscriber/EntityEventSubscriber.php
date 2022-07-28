5de3697970885048e230b1b21b201cf42e134ea5
Mike Decker <pookmish@gmail.com>
2022-07-28 14:50:49 -0700

    Validate menu items for absoluate urls

--- modules/stanford_profile_helper/src/EventSubscriber/EntityEventSubscriber.php
+++ modules/stanford_profile_helper/src/EventSubscriber/EntityEventSubscriber.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\stanford_profile_helper\EventSubscriber;
 
+use Drupal\Core\Entity\ContentEntityInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Installer\InstallerKernel;
 use Drupal\Core\Link;
@@ -11,6 +12,7 @@ use Drupal\Core\State\StateInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\core_event_dispatcher\EntityHookEvents;
 use Drupal\core_event_dispatcher\Event\Entity\EntityPresaveEvent;
+use Drupal\menu_link_content\MenuLinkContentInterface;
 use Drupal\node\NodeInterface;
 use Drupal\preprocess_event_dispatcher\Event\NodePreprocessEvent;
 use Drupal\rabbit_hole\BehaviorInvokerInterface;
@@ -101,21 +103,63 @@ class EntityEventSubscriber implements EventSubscriberInterface {
    *   Triggered Event.
    */
   public function onEntityPresave(EntityPresaveEvent $event): void {
+    $entity_type = $event->getEntity()->getEntityTypeId();
+    $method_name = 'preSave' . str_replace(' ', '', ucwords(str_replace('_', ' ', $entity_type)));
+    // Call individual methods for each entity type if one is available.
+    if (method_exists($this, $method_name)) {
+      $this->{$method_name}($event->getEntity());
+    }
+  }
+
+  /**
+   * Before saving a menu item, adjust the path if an internal path exists.
+   *
+   * @param \Drupal\menu_link_content\MenuLinkContentInterface $entity
+   *   The menu link being saved.
+   */
+  protected function preSaveMenuLinkContent(MenuLinkContentInterface $entity): void {
+    $destination = $entity->get('link')->getString();
+    if ($internal_path = $this->lookupInternalPath($destination)) {
+      $entity->set('link', $internal_path);
+    }
+  }
+
+  /**
+   * Before saving a redirect, adjust the path if an internal path exists.
+   *
+   * @param \Drupal\Core\Entity\ContentEntityInterface $entity
+   *  Redirect to be saved.
+   */
+  protected function preSaveRedirect(ContentEntityInterface $entity): void {
+    $destination = $entity->get('redirect_redirect')->getString();
+    if ($internal_path = $this->lookupInternalPath($destination)) {
+      $entity->set('redirect_redirect', $internal_path);
+    }
+  }
+
+  /**
+   * Before saving a node, if a default content list page exists, create it.
+   *
+   * @param \Drupal\node\NodeInterface $entity
+   *   The node being saved.
+   */
+  protected function preSaveNode(NodeInterface $entity): void {
     if (
       InstallerKernel::installationAttempted() ||
-      !($event->getEntity() instanceof NodeInterface) ||
-      !$event->getEntity()->isNew()
+      !$entity->isNew()
     ) {
       return;
     }
+
     $pages = [
       'stanford_news' => '0b83d1e9-688a-4475-9673-a4c385f26247',
       'stanford_event' => '8ba98fcf-d390-4014-92de-c77a59b30f3b',
       'stanford_person' => '673a8fb8-39ac-49df-94c2-ed8d04db16a7',
       'stanford_course' => '14768832-f763-4d27-8df6-7cd784886d57',
     ];
-    $bundle = $event->getEntity()->bundle();
+    $bundle = $entity->bundle();
     $state_key = 'stanford_profile_helper.default_content.' . $bundle;
+
     if (
       array_key_exists($bundle, $pages) &&
       !$this->state->get($state_key)
@@ -129,11 +173,11 @@ class EntityEventSubscriber implements EventSubscriberInterface {
         ->execute();
 
       if ((int) $count == 0) {
-        $entity = $this->defaultContent->createDefaultContent($pages[$bundle]);
-        if ($entity) {
+        $new_entity = $this->defaultContent->createDefaultContent($pages[$bundle]);
+        if ($new_entity) {
           $this->messenger()
             ->addMessage($this->t('A new page was created automatically for you. View the @link page to make changes.', [
-              '@link' => Link::fromTextAndUrl($entity->label(), $entity->toUrl())
+              '@link' => Link::fromTextAndUrl($new_entity->label(), $new_entity->toUrl())
                 ->toString(),
             ]));
         }
@@ -182,6 +226,32 @@ class EntityEventSubscriber implements EventSubscriberInterface {
       return $this->rabbitHolePluginManager->createInstance($values['rh_action'], $values);
     }
     return NULL;
+  }
+
+  /**
+   * Lookup an internal path.
+   *
+   * @param string $uri
+   *   The destination path.
+   *
+   * @return string|null
+   *   The internal path, or NULL if not found.
+   */
+  protected static function lookupInternalPath(string $uri): ?string {
+    // If a redirect is added to go to the aliased path of a node (often from
+    // importing redirect), change the destination to target the node instead.
+    // This works if the destination is `/about` or `/node/9`.
+    if (preg_match('/^internal:(\/.*)/', $uri, $matches)) {
+      // Find the internal path from the alias.
+      $path = \Drupal::service('path_alias.manager')
+        ->getPathByAlias($matches[1]);
+
+      // Grab the node id from the internal path and use that as the destination.
+      if (preg_match('/node\/(\d+)/', $path, $matches)) {
+        return 'entity:node/' . $matches[1];
+      }
+    }
+    return FALSE;
   }
 
 }
