9840d21106195164a6139dd3541baf70c9c44e99
Mike Decker <pookmish@gmail.com>
2022-07-28 16:57:57 -0700

    move more hooks

--- modules/stanford_profile_helper/src/EventSubscriber/EntityEventSubscriber.php
+++ modules/stanford_profile_helper/src/EventSubscriber/EntityEventSubscriber.php
@@ -2,6 +2,9 @@
 
 namespace Drupal\stanford_profile_helper\EventSubscriber;
 
+use Drupal\config_pages\ConfigPagesInterface;
+use Drupal\Core\Cache\Cache;
+use Drupal\Core\Config\Entity\ConfigEntityInterface;
 use Drupal\Core\Entity\ContentEntityInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Installer\InstallerKernel;
@@ -11,7 +14,11 @@ use Drupal\Core\Routing\TrustedRedirectResponse;
 use Drupal\Core\State\StateInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\core_event_dispatcher\EntityHookEvents;
+use Drupal\core_event_dispatcher\Event\Entity\EntityDeleteEvent;
+use Drupal\core_event_dispatcher\Event\Entity\EntityInsertEvent;
 use Drupal\core_event_dispatcher\Event\Entity\EntityPresaveEvent;
+use Drupal\core_event_dispatcher\Event\Entity\EntityUpdateEvent;
+use Drupal\field\FieldStorageConfigInterface;
 use Drupal\menu_link_content\MenuLinkContentInterface;
 use Drupal\node\NodeInterface;
 use Drupal\preprocess_event_dispatcher\Event\NodePreprocessEvent;
@@ -19,6 +26,7 @@ use Drupal\rabbit_hole\BehaviorInvokerInterface;
 use Drupal\rabbit_hole\Plugin\RabbitHoleBehaviorPluginInterface;
 use Drupal\rabbit_hole\Plugin\RabbitHoleBehaviorPluginManager;
 use Drupal\stanford_profile_helper\StanfordDefaultContentInterface;
+use Drupal\user\RoleInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
@@ -70,6 +78,9 @@ class EntityEventSubscriber implements EventSubscriberInterface {
   public static function getSubscribedEvents() {
     return [
       EntityHookEvents::ENTITY_PRE_SAVE => 'onEntityPresave',
+      EntityHookEvents::ENTITY_INSERT => 'onEntityInsert',
+      EntityHookEvents::ENTITY_UPDATE => 'onEntityUpdate',
+      EntityHookEvents::ENTITY_DELETE => 'onEntityDelete',
       'preprocess_node' => 'preprocessNode',
     ];
   }
@@ -103,11 +114,131 @@ class EntityEventSubscriber implements EventSubscriberInterface {
    *   Triggered Event.
    */
   public function onEntityPresave(EntityPresaveEvent $event): void {
-    $entity_type = $event->getEntity()->getEntityTypeId();
+    $entity = $event->getEntity();
+    $entity_type = $entity->getEntityTypeId();
     $method_name = 'preSave' . str_replace(' ', '', ucwords(str_replace('_', ' ', $entity_type)));
     // Call individual methods for each entity type if one is available.
     if (method_exists($this, $method_name)) {
-      $this->{$method_name}($event->getEntity());
+      $this->{$method_name}($entity);
+    }
+
+    if ($entity instanceof ConfigEntityInterface && $entity->isNew()) {
+      /** @var \Drupal\Core\Config\StorageInterface $config_storage */
+      $config_storage = \Drupal::service('config.storage.sync');
+
+      // The entity exists in the config sync directory, lets check if it's uuid
+      // matches.
+      if (in_array($entity->getConfigDependencyName(), $config_storage->listAll())) {
+        $staged_config = $config_storage->read($entity->getConfigDependencyName());
+
+        // The uuid of the entity doesn't match that of the config in the sync
+        // directory. Make sure they match so that we don't get config sync
+        // issues.
+        if (isset($staged_config['uuid']) && $staged_config['uuid'] != $entity->uuid()) {
+          $entity->set('uuid', $staged_config['uuid']);
+        }
+      }
+    }
+  }
+
+  /**
+   * Before saving a new node, if it's the first one, create a list page.
+   *
+   * @param \Drupal\core_event_dispatcher\Event\Entity\EntityInsertEvent $event
+   *   Triggered Event.
+   */
+  public function onEntityInsert(EntityInsertEvent $event): void {
+    $entity = $event->getEntity();
+    $entity_type = $entity->getEntityTypeId();
+    $method_name = 'insert' . str_replace(' ', '', ucwords(str_replace('_', ' ', $entity_type)));
+    // Call individual methods for each entity type if one is available.
+    if (method_exists($this, $method_name)) {
+      $this->{$method_name}($entity);
+    }
+  }
+
+  /**
+   * Before saving a new node, if it's the first one, create a list page.
+   *
+   * @param \Drupal\core_event_dispatcher\Event\Entity\EntityUpdateEvent $event
+   *   Triggered Event.
+   */
+  public function onEntityUpdate(EntityUpdateEvent $event): void {
+    $entity = $event->getEntity();
+    $entity_type = $entity->getEntityTypeId();
+    $method_name = 'update' . str_replace(' ', '', ucwords(str_replace('_', ' ', $entity_type)));
+    // Call individual methods for each entity type if one is available.
+    if (method_exists($this, $method_name)) {
+      $this->{$method_name}($entity, $event->getOriginalEntity());
+    }
+  }
+
+  /**
+   * Before saving a new node, if it's the first one, create a list page.
+   *
+   * @param \Drupal\core_event_dispatcher\Event\Entity\EntityDeleteEvent $event
+   *   Triggered Event.
+   */
+  public function onEntityDelete(EntityDeleteEvent $event): void {
+    $entity = $event->getEntity();
+    $entity_type = $entity->getEntityTypeId();
+    $method_name = 'delete' . str_replace(' ', '', ucwords(str_replace('_', ' ', $entity_type)));
+    // Call individual methods for each entity type if one is available.
+    if (method_exists($this, $method_name)) {
+      $this->{$method_name}($entity);
+    }
+  }
+
+  protected static function preSaveConfigPages(ConfigPagesInterface $config_page){
+    if (
+      $config_page->hasField('su_site_url') &&
+      ($url_field = $config_page->get('su_site_url')->getValue())
+    ) {
+      // Set the xml sitemap module state to the new domain.
+      \Drupal::state()->set('xmlsitemap_base_url', $url_field[0]['uri']);
+    }
+
+    // Invalidate cache tags on config pages save. This is a blanket cache clear
+    // since config pages mostly affect the entire site.
+    Cache::invalidateTags(['config:system.site', 'system.site', 'block_view', 'node_view']);
+  }
+
+  protected static function preSaveFieldStorageConfig(FieldStorageConfigInterface $field_storage){
+    // If a field is saved and the field permissions are public, lets just remove
+    // those third party settings before save so that it keeps the config clean.
+    if ($field_storage->getThirdPartySetting('field_permissions', 'permission_type') === 'public') {
+      $field_storage->unsetThirdPartySetting('field_permissions', 'permission_type');
+      $field_storage->calculateDependencies();
+    }
+  }
+
+  protected function insertMenuLinkContent(MenuLinkContentInterface $entity){
+    Cache::invalidateTags(['stanford_profile_helper:menu_links']);
+  }
+
+  protected function deleteMenuLinkContent(MenuLinkContentInterface $entity){
+    Cache::invalidateTags(['stanford_profile_helper:menu_links']);
+  }
+
+  protected function updateMenuLinkContent(MenuLinkContentInterface $entity, MenuLinkContentInterface $original_entity){
+    $original = [
+      $original_entity->get('title')->getValue(),
+      $original_entity->get('description')->getValue(),
+      $original_entity->get('link')->getValue(),
+      $original_entity->get('parent')->getValue(),
+      $original_entity->get('weight')->getValue(),
+      $original_entity->get('expanded')->getValue(),
+    ];
+    $updated = [
+      $entity->get('title')->getValue(),
+      $entity->get('description')->getValue(),
+      $entity->get('link')->getValue(),
+      $entity->get('parent')->getValue(),
+      $entity->get('weight')->getValue(),
+      $entity->get('expanded')->getValue(),
+    ];
+    if (md5(json_encode($original)) != md5(json_encode($updated))) {
+      Cache::invalidateTags(['stanford_profile_helper:menu_links']);
     }
   }
 
@@ -117,11 +248,44 @@ class EntityEventSubscriber implements EventSubscriberInterface {
    * @param \Drupal\menu_link_content\MenuLinkContentInterface $entity
    *   The menu link being saved.
    */
-  protected function preSaveMenuLinkContent(MenuLinkContentInterface $entity): void {
+  protected static function preSaveMenuLinkContent(MenuLinkContentInterface $entity): void {
+    $cache_tags = [];
+
     $destination = $entity->get('link')->getString();
-    if ($internal_path = $this->lookupInternalPath($destination)) {
+    if ($internal_path = self::lookupInternalPath($destination)) {
       $entity->set('link', $internal_path);
     }
+
+    // For new menu link items created on a node form (normally), set the expanded
+    // attribute so all menu items are expanded by default.
+    if ($entity->isNew()) {
+      $entity->set('expanded', TRUE);
+    }
+
+    // When a menu item is added as a child of another menu item clear the parent
+    // pages cache so that the block shows up as it doesn't get invalidated just
+    // by the menu cache tags.
+    $parent_id = $entity->getParentId();
+    if (!empty($parent_id)) {
+      [$entity_name, $uuid] = explode(':', $parent_id);
+      $menu_link_content = \Drupal::entityTypeManager()
+        ->getStorage($entity_name)
+        ->loadByProperties(['uuid' => $uuid]);
+
+      if (is_array($menu_link_content)) {
+        $parent_item = array_pop($menu_link_content);
+        /** @var \Drupal\Core\Url $url */
+        $url = $parent_item->getUrlObject();
+        if (!$url->isExternal() && $url->isRouted()) {
+          $params = $url->getRouteParameters();
+          if (isset($params['node'])) {
+            $cache_tags[] = 'node:' . $params['node'];
+          }
+        }
+      }
+    }
+
+    Cache::invalidateTags($cache_tags);
   }
 
   /**
@@ -132,7 +296,7 @@ class EntityEventSubscriber implements EventSubscriberInterface {
    */
   protected function preSaveRedirect(ContentEntityInterface $entity): void {
     $destination = $entity->get('redirect_redirect')->getString();
-    if ($internal_path = $this->lookupInternalPath($destination)) {
+    if ($internal_path = self::lookupInternalPath($destination)) {
       $entity->set('redirect_redirect', $internal_path);
     }
   }
@@ -144,6 +308,10 @@ class EntityEventSubscriber implements EventSubscriberInterface {
    *   The node being saved.
    */
   protected function preSaveNode(NodeInterface $entity): void {
+    // Invalidate any search result cached so the updated/new content will be
+    // displayed for previously searched terms.
+    Cache::invalidateTags(['config:views.view.search']);
+
     if (
       InstallerKernel::installationAttempted() ||
       !$entity->isNew()
@@ -182,6 +350,22 @@ class EntityEventSubscriber implements EventSubscriberInterface {
             ]));
         }
       }
+    }
+  }
+
+  protected static function preSaveUserRole(RoleInterface $role){
+    /** @var \Drupal\Core\Config\StorageInterface $config_storage */
+    $config_storage = \Drupal::service('config.storage.sync');
+
+    // Only modify new roles if they are created through the UI and don't exist in
+    // the config management - Prefix them with "custm_" so they can be easily
+    // identifiable.
+    if (
+      PHP_SAPI != 'cli' &&
+      $role->isNew() &&
+      !in_array($role->getConfigDependencyName(), $config_storage->listAll())
+    ) {
+      $role->set('id', 'custm_' . $role->id());
     }
   }
 
