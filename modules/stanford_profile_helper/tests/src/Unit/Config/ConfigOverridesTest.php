8f85740eb731da3bf32efe08a41532b9913d91eb
pookmish <pookmish@gmail.com>
2022-09-12 08:27:39 -0700

    D8CORE-6217 Allow configuring maximum main menu depth (#611)

--- modules/stanford_profile_helper/tests/src/Unit/Config/ConfigOverridesTest.php
+++ modules/stanford_profile_helper/tests/src/Unit/Config/ConfigOverridesTest.php
@@ -2,18 +2,15 @@
 
 namespace Drupal\Tests\stanford_profile_helper\Unit\Config;
 
+use Drupal\Component\Utility\NestedArray;
 use Drupal\Core\Cache\CacheableMetadata;
-use Drupal\Core\Config\Config;
-use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\State\StateInterface;
+use Drupal\file\FileInterface;
 use Drupal\stanford_profile_helper\Config\ConfigOverrides;
 use Drupal\Tests\UnitTestCase;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\config_pages\ConfigPagesLoaderServiceInterface;
-use Drupal\config_pages\Entity\ConfigPages;
 use Drupal\Core\Entity\EntityStorageInterface;
-use Drupal\file\Entity\File;
-use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\StreamWrapper\StreamWrapperManagerInterface;
 use Drupal\Core\StreamWrapper\StreamWrapperInterface;
 
@@ -25,41 +22,100 @@ use Drupal\Core\StreamWrapper\StreamWrapperInterface;
  */
 class ConfigOverridesTest extends UnitTestCase {
 
-  /**
-   * @var \Drupal\stanford_profile_helper\Config\ConfigOverrides
-   */
-  protected $overrideService;
+  protected $logoFile;
 
-  /**
-   * {@inheritDoc}
-   */
-  protected function setUp(): void {
-    parent::setUp();
-    $state = $this->createMock(StateInterface::class);
-    $state->method('get')->will($this->returnCallback([
-      $this,
-      'getStateCallback',
-    ]));
-
-    $config_pages = $this->createMock(ConfigPagesLoaderServiceInterface::class);
-    $config_factory = $this->createMock(ConfigFactoryInterface::class);
-    $entity_manager = $this->createMock(EntityTypeManagerInterface::class);
-    $stream_wrapper_manager = $this->createMock(StreamWrapperManagerInterface::class);
-
-    $config_factory->method('getEditable')
-      ->will($this->returnCallback([$this, 'getConfigCallback']));
-
-    $this->overrideService = new ConfigOverrides($state, $config_pages, $config_factory, $entity_manager, $stream_wrapper_manager);
-  }
+  protected $configPageValues = [];
 
   /**
    * Custom roles can be assigned by the site managers.
    */
   public function testConfigRolesOverrides() {
-    $overrides = $this->overrideService->loadOverrides(['user.role.site_manager']);
+    $overridder = $this->getOverrideService();
+    $overrides = $overridder->loadOverrides(['user.role.site_manager']);
     $expected['user.role.site_manager']['permissions'][500] = 'assign custm_foo_bar role';
     $expected['user.role.site_manager']['permissions'][501] = 'assign custm_bar_foo role';
     $this->assertEquals($expected, $overrides);
+  }
+
+  /**
+   * Lockup custom overrides through config form.
+   */
+  public function testConfigLockupOverrides() {
+
+    $overridder = $this->getOverrideService();
+    $this->assertEmpty($overridder->loadOverrides(['barfoo.settings']));
+    $this->assertEmpty($overridder->loadOverrides(['foobar.settings']));
+
+    $this->configPageValues['lockup_settings'] = [
+      'su_lockup_enabled' => 0,
+      'su_lockup_options' => 'a',
+      'su_line_1' => 'Line 1',
+      'su_line_2' => 'Line 2',
+      'su_line_4' => 'Line 4',
+      'su_line_5' => 'Line 5',
+      'su_use_theme_logo' => 0,
+      'su_upload_logo_image' => NULL,
+    ];
+
+    $expected = [
+      'foobar.settings' =>
+        [
+          'lockup' => [
+            'option' => 'a',
+            'line1' => 'Line 1',
+            'line2' => 'Line 2',
+            'line4' => 'Line 4',
+            'line5' => 'Line 5',
+          ],
+          'logo' => [
+            'use_default' => FALSE,
+          ],
+        ],
+    ];
+
+    $this->assertEquals($expected, $overridder->loadOverrides(['foobar.settings']));
+
+    $this->configPageValues['lockup_settings']['su_upload_logo_image'] = 1;
+    $this->assertEquals($expected, $overridder->loadOverrides(['foobar.settings']));
+
+    $this->logoFile = $this->createMock(FileInterface::class);
+    $this->logoFile->method('getFileUri')->wilLReturn('public://foobar.jpg');
+
+    $expected['foobar.settings']['logo']['path'] = '/sites/default/files/logo.jpg';
+    $this->assertEquals($expected, $overridder->loadOverrides(['foobar.settings']));
+  }
+
+  public function testMainMenuOverrides() {
+    $configs = [
+      'block.block.foobar_main_menu' => [
+        'plugin' => 'system_menu_block:main',
+        'region' => 'menu',
+      ],
+    ];
+    $overridder = $this->getOverrideService($configs);
+
+    $expected = [];
+    $this->assertEquals($expected, $overridder->loadOverrides(['block.block.foobar_main_menu']));
+
+    $this->configPageValues['stanford_basic_site_settings'] = ['su_site_menu_levels' => 2];
+    $expected['block.block.foobar_main_menu'] = ['settings' => ['depth' => 2]];
+    $this->assertEquals($expected, $overridder->loadOverrides(['block.block.foobar_main_menu']));
+  }
+
+  public function getConfigPageValue($config_name, $field, $delta = [], $key = NULL) {
+    return $this->configPageValues[$config_name][$field] ?? NULL;
+  }
+
+  /**
+   * [testCreateConfigObject description]
+   *
+   * @return [type] [description]
+   */
+  public function testDefaultFunctions() {
+    $overridder = $this->getOverrideService();
+    $this->assertNull($overridder->createConfigObject('name'));
+    $this->assertEquals($overridder->getCacheSuffix(), 'StanfordProfileHelperConfigOverride');
+    $this->assertInstanceOf(CacheableMetadata::class, $overridder->getCacheableMetadata('name'));
   }
 
   /**
@@ -78,194 +134,89 @@ class ConfigOverridesTest extends UnitTestCase {
     return $default_value;
   }
 
-  /**
-   * Lockup custom overrides through config form.
-   */
-  public function testConfigLockupOverrides() {
+  protected function getOverrideService(array $configs = []) {
+    if (!isset($configs['system.theme']['default'])) {
+      $configs['system.theme']['default'] = 'foobar';
+    }
     $state = $this->createMock(StateInterface::class);
+    $state->method('get')->will($this->returnCallback([
+      $this,
+      'getStateCallback',
+    ]));
 
     $config_pages = $this->createMock(ConfigPagesLoaderServiceInterface::class);
-    $config_factory = $this->createMock(ConfigFactoryInterface::class);
+    $config_pages->method('getValue')->will(
+      $this->returnCallback([$this, 'getConfigPageValue'])
+    );
+
+    $file_storage = $this->createMock(EntityStorageInterface::class);
+    $file_storage->method('load')->willReturnReference($this->logoFile);
+
     $entity_manager = $this->createMock(EntityTypeManagerInterface::class);
-    $file = $this->createMock(File::class);
-    $entity_storage = $this->createMock(EntityStorageInterface::class);
-    $stream_wrapper_manager = $this->createMock(StreamWrapperManagerInterface::class);
+    $entity_manager->method('getStorage')->wilLReturn($file_storage);
+
     $stream_wrapper = $this->createMock(StreamWrapperInterface::class);
-
-    $config_factory->method('getEditable')->will(
-      $this->returnCallback([$this, 'getConfigCallback'])
-    );
-
-    $config_factory->method('get')->will(
-      $this->returnCallback([$this, 'getConfigCallback'])
-    );
-
-    $config_pages->method('load')->will(
-      $this->returnCallback([$this, 'getConfigPageLockup'])
-    );
-
-    $file->method('getFileUri')->willReturn('public://logo.jpg');
-    $entity_storage->method('load')->willReturn($file);
-    $entity_manager->method('getStorage')->willReturn($entity_storage);
-
     $stream_wrapper->method('getExternalUrl')
       ->willReturn('/sites/default/files/logo.jpg');
+
+    $stream_wrapper_manager = $this->createMock(StreamWrapperManagerInterface::class);
     $stream_wrapper_manager->method('getViaUri')->willReturn($stream_wrapper);
 
-    $overrideService = new ConfigOverrides($state, $config_pages, $config_factory, $entity_manager, $stream_wrapper_manager);
-
-    $overrides = $overrideService->loadOverrides(['stanford_basic.settings']);
-    $expected = [
-      'stanford_basic.settings' =>
-        [
-          'lockup' => [
-            'option' => 'a',
-            'line1' => 'Line 1',
-            'line2' => 'Line 2',
-            'line3' => 'Line 3',
-            'line4' => 'Line 4',
-            'line5' => 'Line 5',
-          ],
-          'logo' => [
-            'path' => '/sites/default/files/logo.jpg',
-            'use_default' => TRUE,
-          ],
-        ],
-    ];
-    $this->assertEquals($expected, $overrides);
-
-    // TEST SOME FAILURES FOR MORE COVERAGE.
-    // -------------------------------------------------------------------------
-
-    // Test to avoid circular ref.
-    $overrides = $overrideService->loadOverrides(['system.theme']);
-
-    // Test a failed get image from config page.
-    $config_page = $this->createMock(ConfigPages::class);
-    $config_page->method('get')->willReturn(FALSE);
-    $config_pages->method('load')->willReturn($config_page);
-    $overrideService = new ConfigOverrides($state, $config_pages, $config_factory, $entity_manager, $stream_wrapper_manager);
-    $overrides = $overrideService->loadOverrides(['stanford_basic.settings']);
-    $this->assertTrue(is_array($overrides));
-
-    // Test a failed get image fid.
-    $obj = $this->createMock(FieldItemListInterface::class);
-    $obj->method('getValue')->willReturn(FALSE);
-    $obj->method('first')->will(
-      $this->returnSelf()
-    );
-    $config_page = $this->createMock(ConfigPages::class);
-    $config_page->method('get')->willReturn($obj);
-    $config_pages->method('load')->willReturn($config_page);
-    $overrideService = new ConfigOverrides($state, $config_pages, $config_factory, $entity_manager, $stream_wrapper_manager);
-    $overrides = $overrideService->loadOverrides(['stanford_basic.settings']);
-    $this->assertTrue(is_array($overrides));
-
-    // Test a failed config page load.
-    $config_pages->method('load')->willReturn(FALSE);
-    $overrideService = new ConfigOverrides($state, $config_pages, $config_factory, $entity_manager, $stream_wrapper_manager);
-    $overrides = $overrideService->loadOverrides(['stanford_basic.settings']);
-    $this->assertTrue(is_array($overrides));
-
+    $this->configFactory = $this->getConfigFactoryStub([
+      'system.them' => ['stanford_basic' => 0],
+    ]);
+    return new ConfigOverrides($state, $config_pages, $this->getConfigFactoryStub($configs), $entity_manager, $stream_wrapper_manager);
   }
 
-  /**
-   * [getConfigCallback description]
-   *
-   * @param  [type] $name [description]
-   *
-   * @return [type]       [description]
-   */
-  public function getConfigCallback($name) {
-    $config = $this->createMock(Config::class);
-    $setting = [];
-    switch ($name) {
-      case 'core.extension':
-        $setting = ['stable' => 0, 'seven' => 0];
-        break;
+  public function getConfigFactoryStub(array $configs = []) {
+    $config_get_map = [];
+    $config_editable_map = [];
+    // Construct the desired configuration object stubs, each with its own
+    // desired return map.
+    foreach ($configs as $config_name => $config_values) {
+      // Define a closure over the $config_values, which will be used as a
+      // returnCallback below. This function will mimic
+      // \Drupal\Core\Config\Config::get and allow using dotted keys.
+      $config_get = function ($key = '') use ($config_values) {
+        // Allow to pass in no argument.
+        if (empty($key)) {
+          return $config_values;
+        }
+        // See if we have the key as is.
+        if (isset($config_values[$key])) {
+          return $config_values[$key];
+        }
+        $parts = explode('.', $key);
+        $value = NestedArray::getValue($config_values, $parts, $key_exists);
+        return $key_exists ? $value : NULL;
+      };
 
-      case 'system.theme':
-        $setting = ['default' => 'stanford_basic'];
-        break;
+      $immutable_config_object = $this->getMockBuilder('Drupal\Core\Config\ImmutableConfig')
+        ->disableOriginalConstructor()
+        ->getMock();
+      $immutable_config_object->expects($this->any())
+        ->method('get')
+        ->willReturnCallback($config_get);
+      $config_get_map[] = [$config_name, $immutable_config_object];
+
+      $mutable_config_object = $this->getMockBuilder('Drupal\Core\Config\Config')
+        ->disableOriginalConstructor()
+        ->getMock();
+      $mutable_config_object->expects($this->any())
+        ->method('getOriginal')
+        ->willReturnCallback($config_get);
+      $config_editable_map[] = [$config_name, $mutable_config_object];
     }
-
-    $config->method('getOriginal')->willReturn($setting);
-    $config->method('get')->with('default')->willReturn('stanford_basic');
-    return $config;
-  }
-
-  /**
-   * [getConfigPageLockup description]
-   *
-   * @return [type] [description]
-   */
-  function getConfigPageLockup($name) {
-    $config_page = $this->createMock(ConfigPages::class);
-
-    $config_page->method('get')->will(
-      $this->returnCallback([$this, 'getFieldValue'])
-    );
-
-    return $config_page;
-  }
-
-  /**
-   * [getFieldValue description]
-   *
-   * @param  [type] $name [description]
-   *
-   * @return [type]       [description]
-   */
-  public function getFieldValue($name) {
-    $obj = $this->createMock(FieldItemListInterface::class);
-
-    switch ($name) {
-      case 'su_lockup_options':
-        $obj->method('getString')->willReturn('a');
-        break;
-
-      case 'su_line_1':
-        $obj->method('getString')->willReturn('Line 1');
-        break;
-
-      case 'su_line_2':
-        $obj->method('getString')->willReturn('Line 2');
-        break;
-
-      case 'su_line_3':
-        $obj->method('getString')->willReturn('Line 3');
-        break;
-
-      case 'su_line_4':
-        $obj->method('getString')->willReturn('Line 4');
-        break;
-
-      case 'su_line_5':
-        $obj->method('getString')->willReturn('Line 5');
-        break;
-
-      case 'su_use_theme_logo':
-        $obj->method('getString')->willReturn('1');
-        break;
-
-      case 'su_upload_logo_image':
-        $obj->method('getValue')->willReturn([['target_id' => 1]]);
-        break;
-    }
-
-    return $obj;
-  }
-
-  /**
-   * [testCreateConfigObject description]
-   *
-   * @return [type] [description]
-   */
-  function testDefaultFunctions() {
-    $this->assertNull($this->overrideService->createConfigObject('name'));
-    $this->assertEquals($this->overrideService->getCacheSuffix(), 'StanfordProfileHelperConfigOverride');
-    $obj = new CacheableMetadata();
-    $this->assertEquals(get_class($obj), get_class($this->overrideService->getCacheableMetadata('name')));
+    // Construct a config factory with the array of configuration object stubs
+    // as its return map.
+    $config_factory = $this->createMock('Drupal\Core\Config\ConfigFactoryInterface');
+    $config_factory->expects($this->any())
+      ->method('get')
+      ->willReturnMap($config_get_map);
+    $config_factory->expects($this->any())
+      ->method('getEditable')
+      ->willReturnMap($config_editable_map);
+    return $config_factory;
   }
 
 }
