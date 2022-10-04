d8e8cb68f28e9a6625627c87ebaacc9be81365d5
Ian Monroe <ian@ianmonroe.com>
2022-10-04 12:38:39 -0600

    fixup - code climate

--- modules/stanford_courses/stanford_courses.install.php
+++ modules/stanford_courses/stanford_courses.install.php
@@ -6,12 +6,14 @@
  */
 
 /**
- * Drop the migrate_map_stanford_courses table and allow it to be recreated upon the next import.
+ * Drop the migrate_map_stanford_courses table and allow it
+ * to be recreated upon the next import.
  */
 function stanford_courses_update_9001() {
   $database = \Drupal::database();
   if ($database->schema()->tableExists('migrate_map_stanford_courses')) {
-    // We need to remove previously imported courses and allow them to be recreated.
+    // We need to remove previously imported courses and
+    // allow them to be recreated.
     $query = $database->query("SELECT `destid1` FROM {migrate_map_stanford_courses}");
     $result = $query->fetchAll();
     foreach ($result as $obsolete_node_id) {
