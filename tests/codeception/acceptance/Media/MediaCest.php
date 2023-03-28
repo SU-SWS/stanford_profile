8a37412579e14c29b53b2df4bd38e0853a9dfbff
Mike Decker <pookmish@gmail.com>
2023-03-28 13:06:35 -0700

    9.0.0

--- tests/codeception/acceptance/Media/MediaCest.php
+++ tests/codeception/acceptance/Media/MediaCest.php
@@ -143,41 +143,6 @@ class MediaCest {
   }
 
   /**
-   * Specific embed codes are allowed for site managers.
-   *
-   * @group embed-codes
-   */
-  public function testAllowedEmbedCodes(AcceptanceTester $I) {
-    $I->logInWithRole('site_manager');
-    $I->amOnPage('/media/add/embeddable');
-    $I->fillField('Name', $this->faker->words(3, true));
-    $I->fillField('Embed Code', '<iframe src="' . $this->faker->url. '"></iframe>');
-    $I->click('Save');
-    $I->canSee('The given embeddable code is not permitted');
-
-    $I->fillField('Embed Code', 'https://calendar.google.com/foo-bar');
-    $I->click('Save');
-    $I->canSee('The given embeddable code is not permitted');
-
-    $allowed_codes = [
-      '<iframe src="https://calendar.google.com/foo-bar" title="foobar"></iframe>',
-      '<iframe src="https://airtable.com/foo-bar" title="foobar"></iframe>',
-      '<iframe src="https://outlook.office365.com/foo-bar" title="foobar"></iframe>',
-      '<iframe src="https://office365stanford.sharepoint.com/foo-bar" title="foobar"></iframe>',
-      '<iframe src="https://app.smartsheet.com/foo-bar" title="foobar"></iframe>',
-    ];
-
-    foreach ($allowed_codes as $allowed_code) {
-      $I->amOnPage('/media/add/embeddable');
-      $I->fillField('Name', $this->faker->words(3, TRUE));
-      $I->fillField('Embed Code', $allowed_code);
-      $I->click('Save');
-      $I->canSee('has been created');
-    }
-
-  }
-
-  /**
    * Google Form additional field
    */
   public function testForGoogleFormFields(AcceptanceTester $I) {
