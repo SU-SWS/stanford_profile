d7890b4433220e0f481aa10741f17335fe2bc88c
Ian Monroe <ian@ianmonroe.com>
2023-06-27 10:51:12 -0600

    removed template, added preprocessor

--- tests/codeception/functional/Paragraphs/SpacerCest.php
+++ tests/codeception/functional/Paragraphs/SpacerCest.php
@@ -8,32 +8,29 @@
  */
 class SpacerCest {
 
-    /**
-     * The spacer paragraph has one custom field, to set the size of the bottom margin.
-     *
-     */
+  /**
+   * The spacer paragraph has one custom field, to set the size of the bottom margin.
+   */
+  public function testSpacerParagraph(FunctionalTester $I) {
 
-    public function testSpacerParagraph(FunctionalTester $I){
+    $paragraph = $I->createEntity([
+      'type' => 'stanford_spacer',
+    ], 'paragraph');
 
-        $paragraph = $I->createEntity([
-            'type' => 'stanford_spacer',
-            'su_spacer_size' => 'su_spacer_default',
-            ], 'paragraph');
+    $page = $I->createEntity([
+      'type' => 'stanford_page',
+      'title' => $this->faker->text(30),
+      'su_page_components' => [
+        'target_id' => $paragraph->id(),
+        'entity' => $paragraph,
+      ],
+    ]);
 
-        $page = $I->createEntity([
-            'type' => 'stanford_page',
-            'title' => $this->faker->text(30),
-            'su_page_components' => [
-              'target_id' => $paragraph->id(),
-              'entity' => $paragraph,
-            ],
-        ]);
+    $I->logInWithRole('contributor');
+    $I->amOnPage($page->toUrl()->toString());
+    $I->seeElementInDOM('.paragraph--type--stanford-spacer');
+    $I->amOnPage($page->toUrl('edit-form')->toString());
 
-        $I->logInWithRole('contributor');
-        $I->amOnPage($page->toUrl()->toString());
-        $I->seeElementInDOM('.paragraph--type--stanford-spacer');
-        $I->amOnPage($page->toUrl('edit-form')->toString());
-
-    }
+  }
 
 }
