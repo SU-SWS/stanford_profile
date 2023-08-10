049f557c123e63d94739a44d3a38f9c87ed97f0f
Mike Decker <pookmish@gmail.com>
2023-08-10 12:32:25 -0700

    Add safety catch for automated tests with system status check

--- tests/codeception/acceptance/SystemCest.php
+++ tests/codeception/acceptance/SystemCest.php
@@ -14,8 +14,11 @@ class SystemCest {
     $I->logInWithRole('administrator');
     $I->amOnPage('/admin/reports/status');
     $I->canSee('9.5', '.system-status-general-info');
-    $I->canSee('1 Error', '.system-status-counter--error');
-    $I->canSee('Access to update.php ', '.system-status-report__status-icon--error');
+    if ($I->grabMultiple('.system-status-counter--error')) {
+      $I->canSee('1 Error', '.system-status-counter--error');
+      $I->canSee('Access to update.php ', '.system-status-report__status-icon--error');
+    }
+
     if (\Drupal::moduleHandler()->moduleExists('chosen')) {
       $I->canSee('Chosen Javascript file');
       $I->cantSee('Chosen JavaScript file', '.system-status-report__status-icon--error');
