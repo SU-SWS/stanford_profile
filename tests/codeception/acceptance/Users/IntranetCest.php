444f5665ed9b0ea748afa40601e0024bb035d0cd
Mike Decker <pookmish@gmail.com>
2023-09-06 16:11:57 -0700

    added logout step

--- tests/codeception/acceptance/Users/IntranetCest.php
+++ tests/codeception/acceptance/Users/IntranetCest.php
@@ -5,6 +5,7 @@
  *
  * @group users
  * @group no-parallel
+ * @group intranet
  */
 class IntranetCest {
 
@@ -150,9 +151,11 @@ class IntranetCest {
   public function testMediaAccess(AcceptanceTester $I) {
     $I->runDrush('sset stanford_intranet 1');
     $I->runDrush('sset stanford_intranet.allow_file_uploads 1');
+
     $I->logInWithRole('site_manager');
     $I->amOnPage('/media/add/file');
     $I->canSeeResponseCodeIs(200);
+    $I->amOnPage('/user/logout');
 
     $I->runDrush('sset stanford_intranet.allow_file_uploads 0');
     $I->logInWithRole('site_manager');
