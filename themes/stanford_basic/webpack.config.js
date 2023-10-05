205d412637550ba43110f803d2fc240117a76b74
Mike Decker <pookmish@gmail.com>
2023-10-05 15:13:28 -0700

    Updated webpack setup to match stanford_profile_helper

--- themes/stanford_basic/webpack.config.js
+++ themes/stanford_basic/webpack.config.js
@@ -1,190 +1,120 @@
-/* eslint-disable no-undef */
-/**
- * Decanter 6 - Webpack Configuration
- */
 
-// Requires / Dependencies
-const path = require('path');
+const path = require("path");
+const Webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
 const FileManagerPlugin = require('filemanager-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const WebpackAssetsManifest = require('webpack-assets-manifest');
-const TerserPlugin = require('terser-webpack-plugin');
 
-// Paths
-const npmPackage = 'node_modules';
-const srcDir = path.resolve(__dirname, 'src');
-const outputDir = path.resolve(__dirname, 'dist');
-// process.env.NODE_ENV is NOT set, so use the name of the npm script as the clue.
-const devMode = process.env.npm_lifecycle_event !== 'publish';
+const config = {
+  isProd: process.env.NODE_ENV === "production",
+  hmrEnabled: process.env.NODE_ENV !== "production" && !process.env.NO_HMR,
+  distFolder: path.resolve(__dirname, "./dist/css"),
+  wdsPort: 3001,
+};
 
-// Module Exports.
-module.exports = {
-  name: 's',
-  // Define the entry points for which webpack builds a dependency graph.
+var webpackConfig = {
   entry: {
-    "admin":         "/src/scss/admin/index.scss",
-    "base":          "/src/js/base.js",
-    "behaviors":     "/src/js/behaviors.js",
-    "ckeditor":      "/src/scss/ckeditor.scss",
-    "ckeditor5":     "/src/scss/ckeditor5.scss",
-    "components":    "/src/scss/components/index.scss",
-    "layout":        "/src/scss/layout/index.scss",
-    "print":         "/src/scss/print/index.scss",
-    "search-page":   "/src/scss/pages/search/index.scss",
-    "state":         "/src/scss/state/index.scss",
-    "theme":         "/src/scss/theme/index.scss",
-    "user_login":    "/src/scss/admin/user_login.scss",
-    "content/policy":"/src/scss/content/policy/index.scss",
+    "admin":                "/src/scss/admin/index.scss",
+    "base":                 "/src/scss/base/index.scss",
+    "../js/base":           "/src/js/base.js",
+    "../js/behaviors":      "/src/js/behaviors.js",
+    "ckeditor":             "/src/scss/ckeditor.scss",
+    "ckeditor5":            "/src/scss/ckeditor5.scss",
+    "components":           "/src/scss/components/index.scss",
+    "layout":               "/src/scss/layout/index.scss",
+    "print":                "/src/scss/print/index.scss",
+    "search-page":          "/src/scss/pages/search/index.scss",
+    "state":                "/src/scss/state/index.scss",
+    "theme":                "/src/scss/theme/index.scss",
+    "user_login":           "/src/scss/admin/user_login.scss",
+    "content/policy":       "/src/scss/content/policy/index.scss",
   },
-  // Where should I output the assets.
   output: {
+    path: config.distFolder,
     filename: '[name].js',
-    path: path.resolve(__dirname, outputDir + '/js'),
-    clean: true,
-    assetModuleFilename: '../assets/[name][ext]'
+    assetModuleFilename: '../assets/[name][ext][query]'
   },
-  // Allows for map files.
-  devtool: 'source-map',
+  mode: config.isProd ? "production" : "development",
   resolve: {
     alias: {
-      './@fortawesome': path.resolve(__dirname, npmPackage, '@fortawesome'),
-      'basic-assets': path.resolve(__dirname, 'src/assets'),
-      'decanter-assets': path.resolve(__dirname, npmPackage, 'decanter/core/src/img'),
-      'fa-fonts': path.resolve(__dirname, npmPackage, '@fortawesome/fontawesome-free/webfonts')
+      'decanter-assets': path.resolve('node_modules', 'decanter/core/src/img'),
+      'decanter-src': path.resolve('node_modules', 'decanter/core/src'),
+      '@fortawesome': path.resolve('node_modules', '@fortawesome'),
+      'fa-fonts': path.resolve('node_modules', '@fortawesome/fontawesome-free/webfonts')
     }
   },
-  // Optimizations that are triggered by production mode.
-  optimization: {
-    moduleIds: 'deterministic',
-    minimize: !devMode,
-    minimizer: [
-      new CssMinimizerPlugin(),
-      new TerserPlugin()
-    ]
-  },
-  plugins: [
-    // A webpack plugin to manage files before or after the build.
-    // Used here to:
-    // - clean all generated files (js AND css) prior to building
-    // - copy generated files to the styleguide after building
-    // Copying to the styleguide must happen in this build because the 2 configs
-    // run asynchronously, and the kss build finishes before this build generates
-    // the assets that need to be copied.
-    // https://www.npmjs.com/package/filemanager-webpack-plugin
-    new FileManagerPlugin({
-      events: {
-        onStart: {
-          delete: [outputDir + '/**/*']
-        },
-        onEnd: {
-          copy: [
-            {
-              source: npmPackage + "/decanter/core/src/templates/**/*.twig",
-              destination: outputDir + "/templates/decanter/"
-            },
-          ]
-        }
-      }
-    }),
-    // This plugin extracts CSS into separate files. It creates a CSS file per
-    // JS file which contains CSS. It supports On-Demand-Loading of CSS and
-    // SourceMaps.
-    // https://github.com/webpack-contrib/mini-css-extract-plugin
-    new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // both options are optional
-      filename: '../css/[name].css',
-      chunkFilename: '../css/[id].css'
-    }),
-    // This Webpack plugin will generate a JSON file that matches the original
-    // filename with the hashed version.
-    // https://github.com/webdeveric/webpack-assets-manifest
-    new WebpackAssetsManifest({
-      output: 'assets.json'
-    })
-  ],
   module: {
     rules: [
-      // Apply babel ES6 compilation to JavaScript files.
       {
-        test: /\.js$/,
+        test: /\.behavior.js$/,
         exclude: /node_modules/,
+        options: {
+          enableHmr: false
+        },
+        loader: 'drupal-behaviors-loader'
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
         use: {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env']
           }
-        }
+        },
       },
-      // Apply Plugins to SCSS/SASS files.
       {
-        test: /\.s[ac]ss$/,
+        test: /\.(sa|sc|c)ss$/,
         use: [
-          MiniCssExtractPlugin.loader,
-          // CSS Loader. Generate sourceMaps.
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true,
-              url: true
-            }
-          },
-          // Post CSS. Run autoprefixer plugin.
-          {
-            loader: 'postcss-loader',
-            options: {
-              sourceMap: true
-            }
-          },
-          // SASS Loader. Add compile paths to include bourbon.
-          {
-            loader: 'sass-loader',
-            options: {
-              sassOptions: {
-                sourceMap: true,
-                lineNumbers: true,
-                outputStyle: 'nested',
-                precision: 10,
-                includePaths: [
-                  path.resolve(__dirname, npmPackage, 'bourbon/core'),
-                  path.resolve(__dirname, srcDir, 'scss'),
-                  path.resolve(__dirname, npmPackage)
-                ]
-              }
-            }
-          }
+          config.isProd ? { loader: MiniCssExtractPlugin.loader } : 'style-loader',
+          {loader:'css-loader', options: {}},
+          {loader:'postcss-loader', options: {}},
+          {loader:'sass-loader', options: {}}
         ]
       },
-      // Apply plugins to image assets.
       {
-        test: /\.(png|jpg|gif)$/i,
-        type: "asset/resource",
-        generator: {
-          filename: '../assets/img/[name][ext][query]',
-        },
+        test: /\.(png|jpg|gif|svg)$/i,
+        type: "asset"
       },
-      // Apply plugins to svg assets.
       {
-        test: /\.(svg)$/i,
-        type: "asset/resource",
+        test: /\.(woff|woff2|eot|ttf)$/i,
+        type: "asset",
         generator: {
-          filename: '../assets/svg/[name][ext][query]',
-        },
-      },
-      // Apply plugins to font assets.
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
-        generator: {
-          filename: '../assets/fonts/[name][ext][query]',
-        },
-      },
-
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
+          filename: '../assets/fonts/[name][ext][query]'
+        }
       }
+    ]
+  },
+  plugins: [
+    new FixStyleOnlyEntriesPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+    new FileManagerPlugin({
+      events: {
+        onStart: {
+          delete: ["dist"]
+        },
+        onEnd: {
+          copy: [
+            {
+              source: "node_modules/decanter/core/src/templates/**/*.twig",
+              destination: "dist/templates/decanter/"
+            }
+          ],
+        },
+      }
+    }),
+  ],
+  optimization: {
+    minimizer: [
+      new OptimizeCSSAssetsPlugin(),
     ]
   }
 };
+
+if (config.hmrEnabled) {
+  webpackConfig.plugins.push(new Webpack.HotModuleReplacementPlugin());
+}
+module.exports = webpackConfig;
