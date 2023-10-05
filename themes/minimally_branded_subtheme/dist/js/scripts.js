572f27d62ec5075392e9e2153830d833d2d86683
Mike Decker <pookmish@gmail.com>
2023-10-05 15:27:18 -0700

    Update minimally branded subtheme with new webpack

--- themes/minimally_branded_subtheme/dist/js/scripts.js
+++ themes/minimally_branded_subtheme/dist/js/scripts.js
@@ -1,2 +1,27 @@
-!function(n){var r={};function o(e){if(r[e])return r[e].exports;var t=r[e]={i:e,l:!1,exports:{}};return n[e].call(t.exports,t,t.exports,o),t.l=!0,t.exports}o.m=n,o.c=r,o.d=function(e,t,n){o.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:n})},o.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},o.t=function(t,e){if(1&e&&(t=o(t)),8&e)return t;if(4&e&&"object"==typeof t&&t&&t.__esModule)return t;var n=Object.create(null);if(o.r(n),Object.defineProperty(n,"default",{enumerable:!0,value:t}),2&e&&"string"!=typeof t)for(var r in t)o.d(n,r,function(e){return t[e]}.bind(null,r));return n},o.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return o.d(t,"a",t),t},o.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},o.p="",o(o.s=1)}([,function(e,t){window.Drupal.behaviors.minimally_branded_subtheme={attach:function(){jQuery(".su-lockup__wordmark").parents(".su-lockup__cell1").addClass("empty-logo")},detach:function(){}}}]);
-//# sourceMappingURL=scripts.js.map
+/******/ (function() { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/**
+ * Behavior Example that works with Webpack.
+ *
+ * @see: https://www.npmjs.com/package/drupal-behaviors-loader
+ *
+ * Webpack wraps everything in enclosures and hides the global variables from
+ * scripts so special handling is needed.
+ */
+
+/* unused harmony default export */ var __WEBPACK_DEFAULT_EXPORT__ = ({
+  // Attach Drupal Behavior.
+  attach: function attach(context, settings) {
+    // console.log("Attached.");
+    (function ($) {
+      $('.su-lockup__wordmark').parents('.su-lockup__cell1').addClass('empty-logo');
+    })(jQuery);
+  },
+  // Detach Example.
+  detach: function detach() {
+    // console.log("Detached.");
+  }
+});
+/******/ })()
+;
