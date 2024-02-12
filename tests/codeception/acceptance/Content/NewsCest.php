85693ab1ef29cec1ea761a97cbb539addf3bec44
Mike Decker <pookmish@gmail.com>
2024-02-12 13:47:46 -0800

    Adjusted metadata test

--- tests/codeception/acceptance/Content/NewsCest.php
+++ tests/codeception/acceptance/Content/NewsCest.php
@@ -192,7 +192,7 @@ class NewsCest {
     $date_string = \Drupal::service('date.formatter')
       ->format($time, 'custom', 'Y-m-d');
     $metadata_date = \Drupal::service('date.formatter')
-      ->format($time, 'custom', 'D, m/d/Y - 12:00');
+      ->format($time, 'custom', 'Y-m-d');
 
     /** @var \Drupal\node\NodeInterface $node */
     $node = $I->createEntity([
@@ -207,7 +207,7 @@ class NewsCest {
     $I->assertEquals($node->label(), $I->grabAttributeFrom('meta[property="og:title"]', 'content'), 'Metadata "og:title" should match.');
     $I->assertEquals($node->label(), $I->grabAttributeFrom('meta[name="twitter:title"]', 'content'), 'Metadata "twitter:title" should match.');
     $I->assertEquals('article', $I->grabAttributeFrom('meta[property="og:type"]', 'content'), 'Metadata "og:type" should match.');
-    $I->assertEquals($metadata_date, $I->grabAttributeFrom('meta[property="article:published_time"]', 'content'), 'Metadata "article:published_time" should match.');
+    $I->assertEquals("{$metadata_date}T04:00:00-08:00", $I->grabAttributeFrom('meta[property="article:published_time"]', 'content'), 'Metadata "article:published_time" should match.');
 
     $I->cantSeeElement('meta', ['name' => 'description']);
     $I->cantSeeElement('meta', ['property' => 'og:image']);
