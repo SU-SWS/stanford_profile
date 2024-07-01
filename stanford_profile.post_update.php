ce8ff230c257c9d7126e0814174a82f4871c129e
Mike Decker <pookmish@gmail.com>
2024-07-01 11:11:30 -0700

    Update hook for past event and event series pages

--- stanford_profile.post_update.php
+++ stanford_profile.post_update.php
@@ -23,3 +23,28 @@ function stanford_profile_removed_post_updates() {
     'stanford_profile_post_update_site_orgs' => '11.4.0',
   ];
 }
+
+/**
+ * Create default past event and event series node pages if content exists.
+ */
+function stanford_profile_post_update_event_pages() {
+  $node_storage = \Drupal::entityTypeManager()->getStorage('node');
+  $events = $node_storage->getQuery()
+    ->accessCheck(FALSE)
+    ->condition('type', 'stanford_event')
+    ->count()
+    ->execute();
+
+  $default_content_creator = \Drupal::service('stanford_profile_helper.default_content');
+  if ($events) {
+    $default_content_creator->createDefaultContent('86a411a2-0b05-41bc-ae15-2184b8e81ea4');
+  }
+  $event_series = $node_storage->getQuery()
+    ->accessCheck(FALSE)
+    ->condition('type', 'stanford_event-series')
+    ->count()
+    ->execute();
+  if ($event_series) {
+    $default_content_creator->createDefaultContent('ddd5aefb-6b7a-4cd7-aa72-e8c106598bb6');
+  }
+}
