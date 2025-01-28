8a778a044414bbd7c10d870c4794876e9f444f5e
Dena DeBry <denas@stanford.edu>
2025-01-27 18:17:29 -0700

    Update RolesCest.php

--- tests/codeception/acceptance/Users/RolesCest.php
+++ tests/codeception/acceptance/Users/RolesCest.php
@@ -20,6 +20,7 @@ class RolesCest {
     $I->canSee('Site Developer');
     $I->canSee('Administrator');
     $I->canSee('Site Embedder');
+    $I->canSee('Site Reviewer');
   }
 
   /**
@@ -53,6 +54,17 @@ class RolesCest {
     // the admin toolbar.
     $I->amOnPage('/');
     $I->cantSeeElement('#toolbar-administration');
+  }
+
+  /**
+   * Site Reviewer role should be limited to viewing unpublished page.
+   */
+  public function testReviewerRole(AcceptanceTester $I) {
+    $I->logInWithRole('su_site_reviewer');
+    // D8CORE-7622
+    $I->amOnPage('/');
+    $I->canSeeElement('#toolbar-administration');
+    // would be nice to have a test of if they CAN see unpublished pages
   }
 
   /**
