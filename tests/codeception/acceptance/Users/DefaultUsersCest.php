6a675d93acd94a58b047331b9bf26ec5e00d8278
Marc Young <mdyoung3@stanford.edu>
2025-05-20 10:41:45 -0600

    D8CORE-7949 - removed 2 unusued user roles. (#926)

--- tests/codeception/acceptance/Users/DefaultUsersCest.php
+++ tests/codeception/acceptance/Users/DefaultUsersCest.php
@@ -14,8 +14,6 @@ class DefaultUsersCest {
     $I->logInWithRole('administrator');
     $I->amOnPage('/admin/users');
     $I->canSee('Alex');
-    $I->canSee('Jamie');
-    $I->canSee('Sam');
     $I->canSee('Morgan');
     $I->canSee('Kennedy');
   }
