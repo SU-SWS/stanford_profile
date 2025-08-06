049310cbefe918577a303928d577e63b9fa8762a
Mike Decker <pookmish@stanford.edu>
2025-08-06 15:07:36 -0700

    fixup gitpod

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,6 +3,8 @@
 set -ev
 set -o xtrace
 
+PACKAGE=`cat composer.json | jq -r '.name'`
+PROFILE_NAME=`ls | grep info.yml | sed 's/\.info\.yml//'`
 PROFILE_BRANCH=`git rev-parse --abbrev-ref HEAD`
 
 chown -R www-data:www-data /tmp
@@ -11,19 +13,19 @@ git clone --branch 2.x https://github.com/SU-SWS/acsf-cardinalsites-public.git /
 cp .devcontainer/drush.yml /var/www/html/drush/local.drush.yml
 
 cd /var/www/html
-composer require su-sws/stanford_profile:"dev-$PROFILE_BRANCH || $PROFILE_BRANCH-dev" --no-update &&
+composer require "$PACKAGE:dev-$PROFILE_BRANCH || $PROFILE_BRANCH-dev" --no-update &&
 composer update --no-interaction
 rm -rf docroot/*/custom/*
 composer install --prefer-source --no-interaction
 
-rm -rf docroot/profiles/custom/stanford_profile
-ln -snf /workspace/stanford_profile docroot/profiles/custom/stanford_profile
+rm -rf docroot/profiles/custom/$PROFILE_NAME
+ln -snf /workspaces/$PROFILE_NAME docroot/profiles/custom/$PROFILE_NAME
 
 drush sws:multisite:settings
 sed -i "s|uri:.*$|uri: https://$CODESPACE_NAME-80.app.github.dev|" docroot/sites/default/local.drush.yml
 sed -i "s|uri:.*$|uri: https://$CODESPACE_NAME-80.app.github.dev|" drush/local.drush.yml
 
-drush site-install stanford_profile -y -v
+drush site-install $PROFILE_NAME -y -v
 drush cim -y
 
 if [[ ! -z $SSH_PRIVATE_KEY ]]; then
