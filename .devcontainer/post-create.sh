44ac689c4045b4aa889c2f84c50019527fa26cab
Mike Decker <pookmish@stanford.edu>
2025-08-07 09:09:16 -0700

    fixup gitpod

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -3,26 +3,28 @@
 set -ev
 set -o xtrace
 
-PACKAGE=`cat composer.json | jq -r '.name'`
-PROFILE_NAME=`ls | grep info.yml | sed 's/\.info\.yml//'`
-PROFILE_BRANCH=`git rev-parse --abbrev-ref HEAD`
+echo "test setup"
+ls /var/www
+pwd
+ls
 
-chown -R www-data:www-data /tmp
-rm -rf /var/www/html
-cd /workspaces
-
-git clone --branch 2.x https://github.com/SU-SWS/acsf-cardinalsites-public.git stack
-rm -rf $PROFILE_NAME/*
-rsync -r stack/ $PROFILE_NAME
-ln -snf $PROFILE_NAME /var/www/html
-
-#cd $PROFILE_NAME
+#PACKAGE=`cat composer.json | jq -r '.name'`
+#PROFILE_NAME=`ls | grep info.yml | sed 's/\.info\.yml//'`
+#PROFILE_BRANCH=`git rev-parse --abbrev-ref HEAD`
+#
+#chown -R www-data:www-data /tmp
+#rm -rf /var/www/html
+#git clone --branch 2.x https://github.com/SU-SWS/acsf-cardinalsites-public.git /var/www/html
 #cp .devcontainer/drush.yml /var/www/html/drush/local.drush.yml
 #
+#cd /var/www/html
 #composer require "$PACKAGE:dev-$PROFILE_BRANCH || $PROFILE_BRANCH-dev" --no-update &&
 #composer update --no-interaction
 #rm -rf docroot/*/custom/*
 #composer install --prefer-source --no-interaction
+#
+#rm -rf docroot/profiles/custom/$PROFILE_NAME
+#ln -snf /workspaces/$PROFILE_NAME docroot/profiles/custom/$PROFILE_NAME
 #
 #drush sws:multisite:settings
 #sed -i "s|uri:.*$|uri: https://$CODESPACE_NAME-80.app.github.dev|" docroot/sites/default/local.drush.yml
@@ -43,3 +45,4 @@ ln -snf $PROFILE_NAME /var/www/html
 #
 #drush cset stage_file_proxy.settings origin 'localhost' -y
 #drush uli
+
