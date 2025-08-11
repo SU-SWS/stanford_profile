8dffc38e56b388e1eee895938125536929669ccc
Jen Breese-Kauth <jbreese@stanford.edu>
2025-08-11 17:23:12 +0000

    removed excess

--- themes/stanford_basic/algolia-search/src/components/custom-chips.tsx
+++ themes/stanford_basic/algolia-search/src/components/custom-chips.tsx
@@ -1,16 +1,10 @@
 import { useCurrentRefinements } from "react-instantsearch";
 import { type JSX } from 'preact';
 import type { CurrentRefinementsConnectorParamsRefinement } from 'instantsearch.js/es/connectors/current-refinements/connectCurrentRefinements';
-import MobileChipsButton from "./mobile-chips-button";
-import MobileChipsItem from "./mobile-chips-item";
 import ChipsButton from "./chips-button";
 import ChipsItem from "./chips-item";
 
-export type CustomChipsProps = {
-  isMobile?: boolean;
-}
-
-const CustomChips = ({ isMobile = false }:CustomChipsProps) => {
+const CustomChips = () => {
 
   const { items, refine } = useCurrentRefinements();
 
@@ -27,22 +21,6 @@ const CustomChips = ({ isMobile = false }:CustomChipsProps) => {
     });
   });
 
-
-  // Mobile Chips
-  if (isMobile) {
-    return (
-      <>
-      {normalizedItems.map((item, i) => (
-        <MobileChipsItem key={i}>
-          <MobileChipsButton onClick={(e: JSX.TargetedMouseEvent<HTMLButtonElement>) => handleRefine(e, item)} aria-label={`Clear ${item.label} filter`}>{item.label}</MobileChipsButton>
-        </MobileChipsItem>
-      ))}
-      </>
-
-    )
-  }
-
-  // Desktop Chips
   return (
     <>
     {normalizedItems.map((item, i) => (
