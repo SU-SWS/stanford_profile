c161c825736fa98e9615cad6bce56b64272b651a
Jen Breese-Kauth <jbreese@stanford.edu>
2025-08-13 22:35:18 +0000

    simplifying per feedback

--- themes/stanford_basic/algolia-search/src/federated-search-facets.tsx
+++ themes/stanford_basic/algolia-search/src/federated-search-facets.tsx
@@ -63,59 +63,46 @@
 
       return (
         <>
-          {isMobile ? (
-            <div className="mobile-federated-search-facets">
-              <a href="" onClick={toggleVisibility} className="filter-link"><h2>Filter </h2>
-                <i class="fa-solid fa-angle-right"></i>
-              </a>
-
-              {isVisible && (
+          <div className={isMobile ? "mobile-federated-search-facets": "federated-search-facets"}>
+            <fieldset>
+              {isMobile ? (
                 <>
-                  <fieldset>
-                    <div className="legend-title">
-                      <p><i class="fa-solid fa-arrow-left"></i></p>
+                  <a href="" onClick={toggleVisibility} className="filter-link"><h2>Filter </h2>
+                    <i class="fa-solid fa-angle-right"></i>
+                  </a>
+                  <div className="legend-title">
+                    <p><i class="fa-solid fa-arrow-left"></i></p>
                       <legend>Sites</legend>
                     </div>
-                      {sites.map(site =>
-                        <CheckboxLabel key={site.label}>
-                          <input type="checkbox" onChange={() => refineSites(site.value)}
-                                checked={site.isRefined}/>
-                          <span className="checkbox">
-                            <i class="fa-solid fa-check"></i>
-                          </span>
-                          <span className="label-display">{site.label} ({site.count})</span>
-                        </CheckboxLabel>
-                      )}
-                    <div className="mobile-footer-facets">
-                      <ClearFilters/>
-                      <button className="view-results">View Results</button>
-                    </div>
-                  </fieldset>
-
                 </>
-              )}
-          </div>
-          ) : (
-            <div className="federated-search-facets">
-              <h2>Filter by</h2>
-              <ClearFilters/>
-              <fieldset>
-                <legend>Sites</legend>
-                {sites.map(site =>
-                  <CheckboxLabel key={site.label}>
-                    <input type="checkbox" onChange={() => refineSites(site.value)}
-                          checked={site.isRefined}/>
-                    <span className="checkbox">
-                      <i class="fa-solid fa-check"></i>
-                    </span>
-                    <span className="label-display">{site.label} ({site.count})</span>
-                  </CheckboxLabel>
+                ) : (
+                <>
+                  <h2>Filter by</h2>
+                  <legend className="sr-only">Sites</legend>
+                  <ClearFilters/>
+                </>
                 )}
-              </fieldset>
-            </div>
-          )}
+
+              {sites.map(site =>
+                <CheckboxLabel key={site.label}>
+                <input type="checkbox" onChange={() => refineSites(site.value)}
+                      checked={site.isRefined}/>
+                <span className="checkbox">
+                  <i class="fa-solid fa-check"></i>
+                </span>
+                <span className="label-display">{site.label} ({site.count})</span>
+                </CheckboxLabel>
+              )}
+              {isMobile &&
+                <div className="mobile-footer-facets">
+                  <ClearFilters/>
+                  <button className="view-results">View Results</button>
+                </div>
+              }
+            </fieldset>
+          </div>
         </>
       );
-    }
+  }
 
-    export default RefinementSidebar;
+  export default RefinementSidebar;
