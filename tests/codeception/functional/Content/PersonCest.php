403909635855ea2e941d20831ae520456cd5998b
Mike Decker <pookmish@stanford.edu>
2025-08-15 11:45:26 -0700

    added tests

--- tests/codeception/functional/Content/PersonCest.php
+++ tests/codeception/functional/Content/PersonCest.php
@@ -4,10 +4,10 @@ use Codeception\Attribute as CodeceptionAttribute;
 use Faker\Factory;
 
 /**
- * Test opportunity content type.
+ * Test Person content type.
  */
-#[CodeceptionAttribute\Group('opportunity')]
-class OpportunityCest {
+#[CodeceptionAttribute\Group('person')]
+class PersonCest {
 
   /**
    * Faker.
@@ -34,14 +34,16 @@ class OpportunityCest {
     ] = $this->buildTaxonomyTerms($I);
 
     $node = $I->createEntity([
-      'type' => 'stanford_opportunity',
+      'type' => 'stanford_person',
       'title' => $this->faker->words(3, TRUE),
+      'su_person_first_name' => $this->faker->firstName(),
+      'su_person_last_name' => $this->faker->lastName(),
     ]);
 
     $I->logInWithRole('contributor');
 
     $I->amOnPage($node->toUrl('edit-form')->toString());
-    $I->click('#edit-group-taxonomy summary');
+
     $I->canSee($parent_1->label(), 'legend');
     $I->canSee($parent_2->label(), 'legend');
 
@@ -59,99 +61,42 @@ class OpportunityCest {
     $I->selectOption("#$parent_2_id [class*='--level-1'] select.simpler-select", $child_2_2->label());
 
     $I->click('Save');
+    $I->canSeeInCurrentUrl($node->toUrl()->toString());
     $I->canSee($node->label(), 'h1');
-
-    $I->canSee($parent_1->label(), 'h2');
-    $I->canSee($child_1_1->label());
-    $I->canSee($child_1_2->label());
-
-    $I->canSee($parent_2->label(), 'h2');
-    $I->canSee($child_2_1->label());
-    $I->canSee($child_2_2->label());
-  }
-
-  #[CodeceptionAttribute\Group('opportunity-filters')]
-  public function testFilteringOpportunities(FunctionalTester $I) {
-    [
-      $parent_1,
-      $parent_2,
-      $child_1_1,
-      $child_2_1,
-      $child_1_2,
-      $child_2_2,
-    ] = $this->buildTaxonomyTerms($I);
-    $opportunity = $I->createEntity([
-      'type' => 'stanford_opportunity',
-      'title' => $this->faker->words(3, TRUE),
-      'su_opp_tags' => [
-        ['target_id' => $child_1_1->id()],
-        ['target_id' => $child_2_1->id()],
-      ],
-    ]);
-    $paragraph = $I->createEntity([
-      'type' => 'stanford_filtered_lists',
-      'su_list_headline' => $this->faker->words(3, TRUE),
-      'su_filtered_list_view' => [
-        'target_id' => 'stanford_opportunities_filtered',
-        'display_id' => 'cards',
-        'arguments' => '',
-        'items_to_display' => NULL,
-      ],
-    ], 'paragraph');
-    $page = $I->createEntity([
-      'type' => 'stanford_page',
-      'title' => $this->faker->words(3, TRUE),
-      'su_page_components' => [
-        'target_id' => $paragraph->id(),
-        'entity' => $paragraph,
-      ],
-    ]);
-    $I->amOnPage($page->toUrl()->toString());
-    $I->canSee($opportunity->label());
-
-    $I->waitForText($parent_1->label(), 2, 'fieldset');
-
-    $I->checkOption($child_1_2->label());
-    $I->waitForAjaxToFinish();
-    $I->cantSee($opportunity->label());
-
-    $I->checkOption($child_1_1->label());
-    $I->waitForAjaxToFinish();
-    $I->canSee($opportunity->label());
   }
 
   protected function buildTaxonomyTerms(FunctionalTester $I) {
     $parent_1 = $I->createEntity([
-      'vid' => 'opportunity_tag_filters',
+      'vid' => 'person_filters',
       'name' => $this->faker->words(2, TRUE),
       'weight' => 0,
     ], 'taxonomy_term');
     $parent_2 = $I->createEntity([
-      'vid' => 'opportunity_tag_filters',
+      'vid' => 'person_filters',
       'name' => $this->faker->words(2, TRUE),
       'weight' => 10,
     ], 'taxonomy_term');
 
     $child_1_1 = $I->createEntity([
-      'vid' => 'opportunity_tag_filters',
+      'vid' => 'person_filters',
       'name' => $this->faker->words(2, TRUE),
       'parent' => $parent_1->id(),
     ], 'taxonomy_term');
 
     $child_1_2 = $I->createEntity([
-      'vid' => 'opportunity_tag_filters',
+      'vid' => 'person_filters',
       'name' => $this->faker->words(2, TRUE),
       'parent' => $parent_1->id(),
     ], 'taxonomy_term');
 
     $child_2_1 = $I->createEntity([
-      'vid' => 'opportunity_tag_filters',
+      'vid' => 'person_filters',
       'name' => $this->faker->words(2, TRUE),
       'parent' => $parent_2->id(),
     ], 'taxonomy_term');
 
     $child_2_2 = $I->createEntity([
-      'vid' => 'opportunity_tag_filters',
+      'vid' => 'person_filters',
       'name' => $this->faker->words(2, TRUE),
       'parent' => $child_2_1->id(),
     ], 'taxonomy_term');
