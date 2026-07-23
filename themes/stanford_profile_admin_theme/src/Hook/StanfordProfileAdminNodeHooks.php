58af6f1592eec1273c88351b0564b0fbbd152cda
Mike Decker <pookmish@stanford.edu>
2026-07-23 13:20:34 -0700

    change hooks

--- themes/stanford_profile_admin_theme/src/Hook/StanfordProfileAdminNodeHooks.php
+++ themes/stanford_profile_admin_theme/src/Hook/StanfordProfileAdminNodeHooks.php
@@ -4,8 +4,81 @@ declare(strict_types=1);
 
 namespace Drupal\stanford_profile_admin_theme\Hook;
 
-use Drupal\stanford_basic\Hook\StanfordBasicNodeHooks;
+use Drupal\Component\Utility\Html;
+use Drupal\Core\Hook\Attribute\Hook;
+use Drupal\Core\Path\PathMatcherInterface;
 
-class StanfordProfileAdminNodeHooks extends StanfordBasicNodeHooks {
+class StanfordProfileAdminNodeHooks {
+
+  public function __construct(protected PathMatcherInterface $pathMatcher) {}
+
+  #[Hook('preprocess_node')]
+  public function preprocessNode(&$variables) {
+    $node_bundle = $variables['node']->bundle();
+    $variables['#attached']['library'][] = 'stanford_basic/content.' . $node_bundle;
+    unset($variables['content']['book_navigation']);
+
+    if ($variables['view_mode'] == 'full') {
+      $variables['attributes']['class'][] = Html::cleanCssIdentifier('node-page--' . $node_bundle);
+    }
+    else {
+      // Backwards compatibility with UI patterns.
+      $variables['attributes']['class'][] = Html::cleanCssIdentifier('ds-entity--node');
+      $variables['attributes']['class'][] = Html::cleanCssIdentifier('ds-entity--' . $node_bundle);
+      $variables['attributes']['class'][] = Html::cleanCssIdentifier('node--' . $node_bundle);
+    }
+  }
+
+  #[Hook('preprocess_node__stanford_event')]
+  public function preprocessEvent(&$variables) {
+    /** @var \Drupal\node\NodeInterface $node */
+    $node = $variables['node'];
+    $date = $node->get('su_event_date_time')->get(0)?->getValue();
+    $startMonth = $startDay = $endMonth = $endDay = NULL;
+    if ($date) {
+      [
+        $startMonth,
+        $startDay,
+      ] = explode(' ', date('M j', (int) $date['value']));
+      [
+        $endMonth,
+        $endDay,
+      ] = explode(' ', date('M j', (int) $date['end_value']));
+    }
+    $variables['component'] = [
+      'start_month' => $startMonth,
+      'start_date' => $startDay,
+      'end_month' => $endMonth,
+      'end_date' => $endDay,
+    ];
+  }
+
+  #[Hook('preprocess_node__stanford_page')]
+  public function preprocessPage(&$variables) {
+    if (
+      $variables['view_mode'] == 'full' &&
+      $this->pathMatcher->isFrontPage()
+    ) {
+      /** @var \Drupal\node\NodeInterface $node */
+      $node = $variables['node'];
+      if (
+        !$node->get('su_page_banner')->count() &&
+        $node->get('su_page_components')->count() >= 2 &&
+        $node->get('su_page_components')
+          ->get(1)->entity->bundle() == 'stanford_wysiwyg'
+      ) {
+        $variables['attributes']['class'][] = 'add-more-space-to-top';
+      }
+    }
+  }
+
+  #[Hook('preprocess_node__stanford_publication')]
+  public function preprocessPublication(&$variables) {
+    /** @var \Drupal\node\NodeInterface $node */
+    $node = $variables['node'];
+    $citation_type = $node->get('su_publication_citation')
+      ->get(0)?->entity?->getBundleEntity()->label();
+    $variables['component']['super_headline'] = $citation_type ?? 'Publication';
+  }
 
 }
--- themes/stanford_profile_admin_theme/src/Hook/StanfordProfileAdminParagraphHooks.php
+++ themes/stanford_profile_admin_theme/src/Hook/StanfordProfileAdminParagraphHooks.php
@@ -4,8 +4,72 @@ declare(strict_types=1);
 
 namespace Drupal\stanford_profile_admin_theme\Hook;
 
-use Drupal\stanford_basic\Hook\StanfordBasicParagraphHooks;
+use Drupal\Core\Hook\Attribute\Hook;
+use Drupal\Core\Routing\RouteObjectInterface;
 
-class StanfordProfileAdminParagraphHooks extends StanfordBasicParagraphHooks {
+class StanfordProfileAdminParagraphHooks {
+
+  #[Hook('preprocess_paragraph__stanford_banner')]
+  public function preprocessParagraphStanfordBanner(&$variables) {
+    /** @var \Drupal\paragraphs\ParagraphInterface $paragraph */
+    $paragraph = $variables['paragraph'];
+
+    $header_behavior = $paragraph->getBehaviorSetting('hero_pattern', 'heading', 'h2');
+    preg_match('/^(\w+)(.*)$/', $header_behavior, $header_tag);
+
+    $variables['component'] = [
+      'header_tag' => $header_tag[1],
+      'header_classes' => isset($header_tag[2]) ? trim(str_replace('.', ' ', $header_tag[2])) : NULL,
+      'visually_hide_header' => (bool) $paragraph->getBehaviorSetting('hero_pattern', 'hide_heading', FALSE),
+      'overlay_position' => $paragraph->getBehaviorSetting('hero_pattern', 'overlay_position', 'left'),
+      'wrapper_class' => 'bottom-margin-' . $paragraph->getBehaviorSetting('hero_pattern', 'space_below', 'default'),
+    ];
+  }
+
+  #[Hook('preprocess_paragraph__stanford_card')]
+  public function preprocessParagraphStanfordCard(&$variables) {
+    /** @var \Drupal\paragraphs\ParagraphInterface $paragraph */
+    $paragraph = $variables['paragraph'];
+
+    $header_behavior = $paragraph->getBehaviorSetting('su_card_styles', 'heading', 'h2');
+    preg_match('/^(\w+)(.*)$/', $header_behavior, $header_tag);
+    $link_style = $paragraph->getBehaviorSetting('su_card_styles', 'link_style', 'button');
+    $variables['component'] = [
+      'header_tag' => $header_tag[1],
+      'header_classes' => isset($header_tag[2]) ? trim(str_replace('.', ' ', $header_tag[2])) : NULL,
+      'visually_hide_header' => (bool) $paragraph->getBehaviorSetting('su_card_styles', 'hide_heading', FALSE),
+      'link_style' => $link_style,
+    ];
+  }
+
+  #[Hook('preprocess_paragraph__stanford_page_title_banner')]
+  public function preprocessParagraphStanfordPageTitleBanner(&$variables) {
+    $request = \Drupal::request();
+    $route = $request->attributes->get(RouteObjectInterface::ROUTE_OBJECT);
+
+    $page_title = $route ? \Drupal::service('title_resolver')
+      ->getTitle($request, $route) : NULL;
+
+    $variables['page_title'] = $page_title;
+  }
+
+  #[Hook('preprocess_paragraph__stanford_stat_card')]
+  public function preprocessParagraphStanfordStatCard(&$variables) {
+    /** @var \Drupal\paragraphs\ParagraphInterface $paragraph */
+    $paragraph = $variables['paragraph'];
+    $header_behavior = $paragraph->get('su_stat_headline_lvl')
+      ?->getString() ?? 'h2';
+    preg_match('/^(\w+)(.*)$/', $header_behavior, $header_tag);
+    $bg_color = $paragraph->get('su_stat_bg_color')?->getString();
+    $icon_color = $paragraph->get('su_stat_icon_color')?->getString();
+    $stat_color = $paragraph->get('su_stat_stat_color')?->getString();
+    $centered_text = (bool) $paragraph->get('su_stat_centered')?->getString();
+    $variables['component'] = [
+      'bg_color' => $bg_color,
+      'icon_color' => $icon_color,
+      'stat_color' => $stat_color,
+      'centered_text' => $centered_text,
+    ];
+  }
 
 }
