f745412a0554a092e2df8f2dfa2140ad4223594d
Mike Decker <pookmish@stanford.edu>
2026-07-23 14:47:58 -0700

    Adjusted twig logic

--- themes/stanford_basic/src/Hook/StanfordBasicConfigPageHooks.php
+++ themes/stanford_basic/src/Hook/StanfordBasicConfigPageHooks.php
@@ -33,9 +33,6 @@ class StanfordBasicConfigPageHooks {
 
     $login_path = Url::fromRoute('samlauth.saml_controller_login', ['destination' => $redirectDestination]);
 
-    /** @var \Drupal\config_pages\ConfigPagesInterface $config_page */
-    $config_page = $variables['config_pages'];
-
     foreach ($variables['content']['su_local_foot_social'][0]['#items'] as &$link) {
       $url = $link['#url']->toString();
       $host = explode('.', parse_url($url, PHP_URL_HOST));
@@ -46,30 +43,15 @@ class StanfordBasicConfigPageHooks {
         ['#type' => 'html_tag', '#tag' => 'span', '#value' => $link['#title']],
       ];
     }
-    $custom_lockup = !$config_page->get('su_local_foot_use_loc')?->getString();
-    $lockup = $config_page->get('su_local_foot_loc_op')?->getString();
-    $use_default_logo = !!$config_page->get('su_local_foot_use_logo')
-      ?->getString();
+
     $login_url = $this->account->isAnonymous() ? $login_path->toString() : NULL;
     $lockup_title = $this->configFactory->get('system.site')->get('name');
 
     $variables['component'] = [
-      'custom_lockup' => $custom_lockup,
-      'lockup_option' => $lockup,
-      'use_default_logo' => $use_default_logo,
       'weblogin_url' => $login_url,
       'lockup_title' => $lockup_title,
     ];
     $variables['#cache']['contexts'][] = 'url.path';
-  }
-
-  #[Hook('preprocess_config_pages__stanford_global_message')]
-  public function preprocessGlobalMessage(&$variables) {
-    /** @var \Drupal\config_pages\ConfigPagesInterface $config_page */
-    $config_page = $variables['config_pages'];
-    $variables['component'] = [
-      'level' => $config_page->get('su_global_msg_type')->getString(),
-    ];
   }
 
 }
